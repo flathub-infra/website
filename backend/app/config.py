5c1a9f5c8f6e82619e4e4b66b7db570fb1b3f300
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2020-10-22 10:54:29 +0200

    Change default path to ostree repo to user installation

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Flathub API"
-    ostree_repo: str = "/var/lib/flatpak/repo"
+    ostree_repo: str = "/root/.local/share/flatpak/repo"
     redis_host: str = "localhost"
     redis_port: int = 6379
     github_token: Optional[str] = None
