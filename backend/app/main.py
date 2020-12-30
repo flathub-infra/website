67cdaa768f1eab389044ffc5354248af15ed6db8
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2020-12-30 20:41:56 +0100

    Drop /apps endpoint and prefix

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,15 +26,13 @@ def startup_event():
     apps.initialize()
 
 
-@app.post("/apps/update")
+@app.post("/update")
 def update_apps(background_tasks: BackgroundTasks):
     ret = apps.update_apps(background_tasks)
     picks.update()
     return ret
 
 
-# TODO: should be optimized/cached, it's fairly slow at 23 req/s
-@app.get("/apps")
 @lru_cache()
 def list_apps_summary(
     index: str = "apps:index", appids: Tuple[str, ...] = None, sort: bool = True
@@ -45,18 +43,9 @@ def list_apps_summary(
     return ret
 
 
-@app.get("/apps/category/{category}")
+@app.get("/category/{category}")
 def list_apps_in_category(category: schemas.Category):
     return list_apps_summary(f"categories:{category}", appids=None, sort=True)
-
-
-@app.get("/apps/{appid}", status_code=status.HTTP_200_OK)
-def get_app(appid: str, response: Response):
-    app = apps.get_app(appid)
-    if not app:
-        response.status_code = status.HTTP_204_NO_CONTENT
-        return response
-    return app
 
 
 @app.get("/appstream/{appid}")
@@ -64,13 +53,13 @@ def get_appid_appstream(appid: str, repo: str = "stable"):
     return apps.get_appid_appstream(appid, repo)
 
 
-@app.get("/apps/search/{userquery}")
+@app.get("/search/{userquery}")
 def search(userquery: str):
     return apps.search(userquery)
 
 
-@app.get("/apps/collection/recently-updated")
-@app.get("/apps/collection/recently-updated/{limit}")
+@app.get("/recently-updated")
+@app.get("/recently-updated/{limit}")
 @lru_cache()
 def get_recently_updated(limit: int = 100):
     return apps.get_recently_updated(limit)
