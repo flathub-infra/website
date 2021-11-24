26cc8bdb98d9a398eb4b1db77fa64b049d8234b0
Kolja Lampe <razzeee@gmail.com>
2021-11-25 00:43:39 +0100

    Rename stats page to statistics page
    Also use css variables where possible

--- frontend/pages/statistics.tsx
+++ frontend/pages/statistics.tsx
@@ -4,8 +4,8 @@ import WorldMap from 'react-svg-worldmap'
 import { useTheme } from 'next-themes'
 import { GetStaticProps } from 'next'
 import { fetchStats } from '../src/fetchers'
-import { Stats } from '../src/types/Stats'
-import styles from './stats.module.scss'
+import { Stats as Statistics } from '../src/types/Stats'
+import styles from './statistics.module.scss'
 import 'chart.js/auto'
 import { Line } from 'react-chartjs-2'
 import { chartOptions, chartStyle } from '../src/chartHelper'
@@ -17,7 +17,7 @@ import {
 } from 'react-icons/md'
 import ListBox from '../src/components/application/ListBox'
 
-const Stats = ({ stats }: { stats: Stats }): JSX.Element => {
+const Statistics = ({ stats }: { stats: Statistics }): JSX.Element => {
   let country_data: { country: string; value: number }[] = []
   if (stats.countries) {
     for (const [key, value] of Object.entries(stats.countries)) {
@@ -27,9 +27,7 @@ const Stats = ({ stats }: { stats: Stats }): JSX.Element => {
 
   const { resolvedTheme } = useTheme()
 
-  const borderColor = resolvedTheme === 'dark' ? '#FFFFFF' : '#000000'
   const gridColor = resolvedTheme === 'dark' ? '#3d3d3d' : '#e5e5e5'
-  const backgroundColor = resolvedTheme === 'dark' ? '#202020' : 'white'
 
   let downloads_labels: string[] = []
   let downloads_data: number[] = []
@@ -50,9 +48,9 @@ const Stats = ({ stats }: { stats: Stats }): JSX.Element => {
 
   return (
     <Main>
-      <NextSeo title='Stats' description='Flathub Stats' />
+      <NextSeo title='Statistics' description='Flathub Statistics' />
       <div className='main-container'>
-        <h1>Flathub Stats</h1>
+        <h1>Statistics</h1>
         <div className={styles.stats}>
           <ListBox
             items={[
@@ -95,9 +93,9 @@ const Stats = ({ stats }: { stats: Stats }): JSX.Element => {
         <h3>Downloads per country</h3>
         <div className={styles.map}>
           <WorldMap
-            color='#52006a'
-            backgroundColor={backgroundColor}
-            borderColor={borderColor}
+            color='var(--main-color)'
+            backgroundColor='var(--bg-color-secondary)'
+            borderColor='var(--text-primary)'
             valueSuffix='downloads'
             size='responsive'
             data={country_data}
@@ -123,4 +121,4 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-export default Stats
+export default Statistics
