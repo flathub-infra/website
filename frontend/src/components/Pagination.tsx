8015150266f95d099630e2643ac2626bc2d1ccce
Kolja Lampe <razzeee@gmail.com>
2021-12-26 00:04:05 +0100

    Improve paging

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -16,28 +16,42 @@ const Pagination: FunctionComponent<Props> = ({ currentPage, pages }) => {
 
   return (
     <div className={styles.pagination}>
-      {pages.map((p, index) => {
-        const isActive = p === currentPage
-        const className = isActive ? `${styles.pageActive}` : ''
-
-        return (
-          <a
-            onClick={() => {
-              router.push({
-                pathname: router.pathname,
-                query: {
-                  ...router.query,
-                  page: p.toString(),
-                },
-              })
-            }}
-            key={`pagination-${index}`}
-            className={`${className} ${styles.pageLink}`}
-          >
-            {p}
-          </a>
+      {pages
+        .filter(
+          (page) =>
+            (currentPage - 3 < page && currentPage + 3 > page) ||
+            pages[0] === page ||
+            pages.length === page
         )
-      })}
+        .map((curr, index, array) => {
+          const isActive = curr === currentPage
+          const className = isActive ? `${styles.pageActive}` : ''
+
+          return (
+            <>
+              {index > 0 && array[index - 1] + 1 !== curr && (
+                <div className={`${styles.pageLink} ${styles.ellipsis}`}>
+                  ...
+                </div>
+              )}
+              <a
+                onClick={() => {
+                  router.push({
+                    pathname: router.pathname,
+                    query: {
+                      ...router.query,
+                      page: curr.toString(),
+                    },
+                  })
+                }}
+                key={`pagination-${index}`}
+                className={`${className} ${styles.pageLink}`}
+              >
+                {curr}
+              </a>
+            </>
+          )
+        })}
     </div>
   )
 }
