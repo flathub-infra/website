ee67ee2f834b2850688989631899f73eec5c0342
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2022-01-27 09:32:07 +0000

    (models): Add model updates for github logins
    Signed-off-by: Daniel Silverstone <daniel.silverstone@codethink.co.uk>

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,14 +17,20 @@ class GithubAccount(Base):
     __tablename__ = "githubaccount"
 
     id = Column(Integer, primary_key=True)
-    user = ForeignKey(FlathubUser.id, nullable=False, index=True)
+    user = Column(Integer, ForeignKey(FlathubUser.id), nullable=False, index=True)
     github_userid = Column(Integer, nullable=False)
+    login = Column(String)
+    avatar_url = Column(String)
     token = Column(String, nullable=True, default=None)
     last_used = Column(DateTime, nullable=True, default=None)
 
     @staticmethod
     def by_user(db, user: FlathubUser):
-        db.session.query(GithubAccount).filter(GithubAccount.user == user.id).first()
+        return db.session.query(GithubAccount).filter_by(user=user.id).first()
+
+    @staticmethod
+    def by_gh_id(db, ghid):
+        return db.session.query(GithubAccount).filter_by(github_userid=ghid).first()
 
 
 class GithubFlowToken(Base):
@@ -31,3 +39,33 @@ class GithubFlowToken(Base):
     id = Column(Integer, primary_key=True)
     state = Column(String, nullable=False)
     created = Column(DateTime, nullable=False)
+
+
+class GithubRepository(Base):
+    __tablename__ = "githubrepository"
+
+    id = Column(Integer, primary_key=True)
+    github_account = Column(
+        Integer, ForeignKey(GithubAccount.id), nullable=False, index=True
+    )
+    reponame = Column(String, nullable=False)
+
+    @staticmethod
+    def unify_repolist(db, account: GithubAccount, repolist: List[str]):
+        all_names = set(repolist)
+        existing = db.session.query(GithubRepository).filter_by(
+            github_account=account.id
+        )
+        to_remove = [repo for repo in existing if repo.reponame not in all_names]
+        existing_names = set(repo.reponame for repo in existing)
+        to_add = [repo for repo in repolist if repo not in existing_names]
+        for repo in to_remove:
+            db.session.delete(repo)
+        for repo in to_add:
+            new_repo = GithubRepository(github_account=account.id, reponame=repo)
+            db.session.add(new_repo)
+        db.session.flush()
+
+    @staticmethod
+    def all_by_account(db, account: GithubAccount):
+        return db.session.query(GithubRepository).filter_by(github_account=account.id)
