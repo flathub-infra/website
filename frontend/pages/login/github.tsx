8151e4e36dbbb5b580aa83128e12862fe2d6a10b
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-02-03 14:32:48 +0000

    Move login request to a context action
    - Separates request handling responsibility out of component, better
      abstraction.
    - Easier flow for redirect to userpage on success.

--- frontend/pages/login/github.tsx
+++ frontend/pages/login/github.tsx
@@ -1,69 +1,39 @@
+import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Main from '../../src/components/layout/Main';
-import { getUserData } from '../../src/context/actions';
-import { useUserDispatch } from '../../src/context/user-info';
-import { LOGIN_PROVIDERS_URL } from '../../src/env';
-import Error from '../_error';
+import { login } from '../../src/context/actions';
+import { useUserContext, useUserDispatch } from '../../src/context/user-info';
 
 export default function AuthReturnPage() {
   // Must access query params to POST to backend for verification with GitHub
   const router = useRouter()
 
-  // Use state to rerender when request resolves
-  const [status, setStatus] = useState(null)
-  const [feedback, setFeedback] = useState('Awaiting server')
-
+  const user = useUserContext()
   const dispatch = useUserDispatch()
+
+  // Redirect to userpage once logged in
+  if (user.info) {
+    router.push('/userpage')
+  }
 
   useEffect(() => {
     // Router must be ready to access query parameters
     // This must be a dependency to ensure only runs once
     if (!router.isReady) { return }
 
-    // Show 404 if user tries some kind of directory traversal
+    // Redirect to home if user tries some kind of directory traversal
     if (router.query.code == null || router.query.state == null) {
-      setStatus(404)
+      router.push('/')
       return
     }
 
-    const postRequest = async (url: string) => {
-      const res = await fetch(url, {
-        method: 'POST',
-        credentials: 'include', // Must use the session cookie
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          code: router.query.code,
-          state: router.query.state,
-        })
-      })
+    login(dispatch, router.query)
+  }, [router, dispatch])
 
-      if (res.ok) {
-        // May just want to redirect on successful login
-        getUserData(dispatch)
-        setFeedback('Success')
-      } else {
-        // Bad response codes
-        // Note: Some of these come with an error message from backend we could display
-        setStatus(res.status)
-      }
-    }
-
-    // Catches any unexpected network errors
-    postRequest(`${LOGIN_PROVIDERS_URL}/github`)
-      .catch(() => setFeedback('Network error, please try again'))
-  }, [router])
-
-  if (status) {
-    return <Error statusCode={status}></Error>
-  }
-
-  // Only show error page if status is set
   return (
     <Main>
-      <p>{feedback}</p>
+      <NextSeo title='Login' noindex={true}></NextSeo>
     </Main>
   )
 }
--- frontend/src/context/actions.ts
+++ frontend/src/context/actions.ts
@@ -1,6 +1,30 @@
+import { ParsedUrlQuery } from "querystring";
 import { Dispatch } from "react";
-import { USER_INFO_URL } from "../env";
+import { LOGIN_PROVIDERS_URL, USER_INFO_URL } from "../env";
 import { UserStateAction } from "../types/Login";
+
+export async function login(dispatch: Dispatch<UserStateAction>, query: ParsedUrlQuery) {
+  dispatch({type: 'loading'})
+
+  const res = await fetch(`${LOGIN_PROVIDERS_URL}/github`, {
+    method: 'POST',
+    credentials: 'include', // Must use the session cookie
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      code: query.code,
+      state: query.state,
+    })
+  })
+
+  if (res.ok) {
+    getUserData(dispatch)
+  } else {
+    // Note: Some of these come with an error message from backend we could display
+    dispatch({type: 'logout'})
+  }
+}
 
 export async function getUserData(dispatch: Dispatch<UserStateAction>) {
   // Indicate the user data is being fetched
