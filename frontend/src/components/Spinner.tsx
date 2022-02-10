f3253525d995d2a569c3ce6c09972440a3d55f7f
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-02-10 13:52:54 +0000

    Add spinner component for loading states
    - A circle with a spinning outer arc, can be used to reflect unsreolved
      state of async operations.
    - Displays loading text by default for explicit user feedback on the
      operation in progress. Can be changed as a component prop.

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,31 @@
+import { FunctionComponent } from 'react'
+import styles from './Spinner.module.scss'
+
+interface Props {
+  size: number, // Size in pixels
+  text?: string, // Message to display underneath
+}
+
+const Spinner: FunctionComponent<Props> = ({ size, text='Loading...' }) => {
+
+  // SVG of a circle with a highlighted 90 degree arc on the border
+  // Made to spin via CSS animation
+  return (
+    // Spinner will always be centered and padded relative to its size
+    <div className={styles.container} style={{ padding: `${size/2}px` }}>
+      <svg className={styles.spinner}
+        width={size}
+        height={size}
+        viewBox='0 0 100 100'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <circle cx='50' cy='50' r='50' className={styles.back} />
+        <path className={styles.front} d='M50,50l0,50a50,50 0 0 1 -50,-50z' />
+        <circle cx='50' cy='50' r='45' className={styles.center} />
+      </svg>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default Spinner
