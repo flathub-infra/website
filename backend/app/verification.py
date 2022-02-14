a6b6bf747a116368b03befdbfa4495e1b20a25ef
James Westman <james@jwestman.net>
2022-02-14 11:08:12 -0600

    verification: Add documentation comments

--- backend/app/verification.py
+++ backend/app/verification.py
@@ -61,6 +61,19 @@ def _get_domain_name(appid: str) -> str:
 
 
 def _check_app_id_error(appid: str) -> str:
+    """
+    Returns an error detail if the app ID is invalid, or None if it is valid.
+
+    All error details, from this function and others:
+    - "malformed_app_id": The app ID is not syntactically correct
+    - "repo_does_not_exist": The app ID does not refer to an existing flathub repository.
+    - "error_connecting_to_github": The server could not connect to GitHub.
+    - "invalid_domain": Website verification was requested, but the app ID is not eligible for website verification.
+    - "failed_to_connect": The server could not connect to the website.
+    - "server_returned_error": The server got a non-200 status code from the website.
+    - "app_not_listed": The app ID is not listed in /.well-known/org.flathub.VerifiedApps.txt on the website.
+    - "blocked_by_admins": The app cannot be verified because the flathub administrators manually blocked it.
+    """
     if len(appid.split(".")) < 3:
         return "malformed_app_id"
     elif not re.match("[_\w\.]+$", appid):
@@ -115,6 +128,17 @@ def _check_website_verification(appid: str):
 
 
 def get_verification_methods(appid: str):
+    """
+    Gets a list of all available verification methods for an app.
+
+    Returns:
+    - "methods": An array of methods.
+      - "method": "website" or "login_provider"
+      - "website": For "website" method, the domain name for the website (e.g. flathub.org)
+      - "login_provider": For "login_provider" method. Currently only "GitHub".
+      - "login_name": For "login_provider" method. The username of the user who must verify the app.
+    - "detail": Error detail.
+    """
     if detail := _check_app_id_error(appid):
         return {
             "methods": [],
@@ -152,6 +176,15 @@ def get_verification_methods(appid: str):
 
 
 def get_verification_status(appid: str):
+    """
+    Gets the verification status of an app ID.
+
+    Returns:
+    - "verified": True or False
+    - "method": "none", "manual", or "website"
+    - "website": For "website" method, the domain name of the website that verified the app (e.g. "gnome.org").
+    - "detail": Error detail. See docs for _check_app_id_error().
+    """
     if detail := _check_app_id_error(appid):
         return {
             "verified": False,
@@ -185,6 +218,15 @@ def get_verification_status(appid: str):
 
 
 def get_website_verification(appid: str):
+    """
+    Returns information helpful for debugging the website verification process, such as what error occurred (if any)
+    and the status code received from the website.
+
+    Returns:
+    - "verified": True or False
+    - "detail": Error detail. See docs for _check_app_id_error().
+    - "status_code": The status code if "detail" is "server_returned_error".
+    """
     if detail := _check_app_id_error(appid):
         return {
             "verified": False,
