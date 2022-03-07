fa50600a0a0b0bd21148ecfcd44c3356e4d0bca2
Kolja Lampe <razzeee@gmail.com>
2022-03-07 22:50:45 +0100

    If the search returns empty, show something more helpful

--- frontend/pages/apps/search/[query].tsx
+++ frontend/pages/apps/search/[query].tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'next-i18next'
+import { Trans, useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
@@ -17,11 +17,22 @@ export default function Search() {
     <Main>
       <NextSeo title={t('search-for-query', { query })} />
 
-      {searchResult && (
+      {searchResult && searchResult.length > 0 && (
         <Collection
           title={t('search-for-query', { query })}
           applications={searchResult}
         />
+      )}
+      {!searchResult || searchResult.length === 0 && (
+        <div className='main-container'>
+          <h2>{t('search-for-query', { query })}</h2>
+          <p>{t('could-not-find-match-for-search')}</p>
+          <p>
+            <Trans i18nKey={"common:request-new-app"}>
+              If you&apos;re searching for a specific application, let the community know, that you want it on flathub <a target='_blank' rel='noreferrer' href='https://discourse.flathub.org/t/about-the-requests-category/22'>here</a>.
+            </Trans>
+          </p>
+        </div>
       )}
     </Main>
   )
