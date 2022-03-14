0890dfbf5bd33d6403664298d16ef71b0252338c
Kolja Lampe <razzeee@gmail.com>
2022-03-14 17:05:10 +0100

    Add new locales

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,7 +1,9 @@
+// @ts-check
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de', 'fr', 'nb_NO', 'tr'],
+    locales: ['en', 'de', 'fr', 'nb_NO', 'tr', 'fi', 'id', 'it', 'pl', 'pt_BR'],
   },
   // debug: true,
   // saveMissing: true,
--- frontend/src/localize.ts
+++ frontend/src/localize.ts
@@ -1,36 +1,57 @@
-
-import { de, enUS, tr, fr, nb } from 'date-fns/locale'
+import { de, enUS, tr, fr, nb, fi, id, pl, pt, it } from 'date-fns/locale'
 
 export function getLocale(language: string): Locale {
-    switch (language) {
-        case 'de':
-            return de
-        case 'en':
-            return enUS
-        case 'fr':
-            return fr
-        case 'nb_NO':
-            return nb
-        case 'tr':
-            return tr
-        default:
-            return enUS
-    }
+  switch (language) {
+    case 'de':
+      return de
+    case 'en':
+      return enUS
+    case 'fr':
+      return fr
+    case 'nb_NO':
+      return nb
+    case 'tr':
+      return tr
+    case 'fi':
+      return fi
+    case 'id':
+      return id
+    case 'it':
+      return it
+    case 'pl':
+      return pl
+    case 'pt_BR':
+      return pt
+
+    default:
+      return enUS
+  }
 }
 
 export function getLocaleString(language: string): string {
-    switch (language) {
-        case 'de':
-            return "de_DE"
-        case 'en':
-            return "en_US"
-        case 'fr':
-            return "fr_FR"
-        case 'nb_NO':
-            return "nb_NO"
-        case 'tr':
-            return "tr_TR"
-        default:
-            return "en_US"
-    }
+  switch (language) {
+    case 'de':
+      return 'de_DE'
+    case 'en':
+      return 'en_US'
+    case 'fr':
+      return 'fr_FR'
+    case 'nb_NO':
+      return 'nb_NO'
+    case 'tr':
+      return 'tr_TR'
+    case 'fi':
+      return 'fi_FI'
+    case 'id':
+      return 'id_ID'
+    case 'it':
+      return 'it_IT'
+    case 'pl':
+      return 'pl_PL'
+    case 'pt_BR':
+      return 'pt_BR'
+
+    default:
+      return 'en_US'
+  }
 }
