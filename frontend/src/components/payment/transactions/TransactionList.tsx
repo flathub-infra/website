eacb653666a6a82dd3ee78169d410e369935aecb
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-03-30 14:21:24 +0100

    Improve transaction list component
    - Add buttons to row to afford where action is needed
    - Include ID as user may need to reference this
    - Align content to vertical center of row to match button
    - Declare component for each row of the list in the same file since
      they're so heavily coupled

--- frontend/src/components/payment/transactions/TransactionList.tsx
+++ frontend/src/components/payment/transactions/TransactionList.tsx
@@ -1,22 +1,28 @@
+import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
 import { FunctionComponent } from 'react'
 import { Transaction } from '../../../types/Payment'
-import TransactionInfo from './TransactionInfo'
+import Button from '../../Button'
 import styles from './TransactionList.module.scss'
 
-interface Props {
+interface ListProps {
   transactions: Transaction[]
 }
 
-const TransactionList: FunctionComponent<Props> = ({ transactions }) => {
+const TransactionList: FunctionComponent<ListProps> = ({ transactions }) => {
+  const { t } = useTranslation()
+
   return (
     <table className={styles.transactionList}>
       <thead>
         <tr>
-          <td>Creation</td>
-          <td>Updated</td>
-          <td>Type</td>
-          <td>Value</td>
-          <td>Status</td>
+          <td>{t('created')}</td>
+          <td>{t('updated')}</td>
+          <td>{t('id')}</td>
+          <td>{t('type')}</td>
+          <td>{t('value')}</td>
+          <td>{t('status')}</td>
+          <td>{t('actions')}</td>
         </tr>
       </thead>
       <tbody>
@@ -26,11 +32,56 @@ const TransactionList: FunctionComponent<Props> = ({ transactions }) => {
           ))
         ) : (
           <tr>
-            <td>No transaction history to show.</td>
+            <td>{t('no-transaction-history')}</td>
           </tr>
         )}
       </tbody>
     </table>
+  )
+}
+
+interface RowProps {
+  transaction: Transaction
+}
+
+const TransactionInfo: FunctionComponent<RowProps> = ({ transaction }) => {
+  const { t, i18n } = useTranslation()
+
+  const { id, created, updated, kind, value, status } = transaction
+
+  const prettyCreated = new Date(created * 1000).toLocaleString()
+  const prettyUpdated = new Date(updated * 1000).toLocaleString()
+  const prettyValue = new Intl.NumberFormat(i18n.language, {
+    style: 'currency',
+    currency: 'USD',
+    currencyDisplay: 'symbol',
+  }).format(value / 100)
+
+  const needsAttention =
+    transaction.status === 'new' || transaction.status === 'retry'
+
+  // Date object expects milliseconds since epoch
+  return (
+    <tr className={styles.info}>
+      <td>{prettyCreated}</td>
+      <td>{prettyUpdated}</td>
+      <td>{id}</td>
+      <td>{kind}</td>
+      <td>{prettyValue}</td>
+      <td>{status}</td>
+      <td className={styles.actions}>
+        <Link
+          href={`/payment/${
+            needsAttention ? transaction.id : `details/${transaction.id}`
+          }`}
+          passHref
+        >
+          <Button type={needsAttention ? 'primary' : 'secondary'}>
+            {needsAttention ? t('checkout') : t('view')}
+          </Button>
+        </Link>
+      </td>
+    </tr>
   )
 }
 
