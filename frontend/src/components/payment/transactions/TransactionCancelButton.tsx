04773b3a489593a360de99427ce0a2272dc94a0c
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-04-08 19:52:36 +0100

    Handle error on transaction cancellation

--- frontend/src/components/payment/transactions/TransactionCancelButton.tsx
+++ frontend/src/components/payment/transactions/TransactionCancelButton.tsx
@@ -4,11 +4,24 @@ import { toast } from 'react-toastify'
 import { TRANSACTION_CANCEL_URL } from '../../../env'
 import Button from '../../Button'
 
+/**
+ * Performs API request to cancel an active transaction
+ * @param transactionId ID of the transaction to cancel
+ */
 async function cancelTransaction(transactionId: string) {
-  const res = await fetch(TRANSACTION_CANCEL_URL(transactionId), {
-    method: 'POST',
-    credentials: 'include',
-  })
+  let res: Response
+  try {
+    res = await fetch(TRANSACTION_CANCEL_URL(transactionId), {
+      method: 'POST',
+      credentials: 'include',
+    })
+  } catch {
+    throw 'network-error-try-again'
+  }
+
+  if (!res.ok) {
+    throw 'network-error-try-again'
+  }
 }
 
 interface Props {
