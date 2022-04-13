danielsilverstone-ct/frontend

Add transaction history rendering

pages/wallet.tsx
@@ -5,6 +5,7 @@ import Main from '../src/components/layout/Main'
 import Spinner from '../src/components/Spinner'
 import UserDetails from '../src/components/user/Details'
 import SavedCards from '../src/components/user/wallet/SavedCards'
+import TransactionHistory from '../src/components/user/wallet/TransactionHistory'
 import { useUserContext } from '../src/context/user-info'
 import styles from './userpage.module.scss'
 
@@ -28,6 +29,7 @@ export default function Wallet() {
     <div className={styles.userArea}>
       <UserDetails />
       <SavedCards />
+      <TransactionHistory />
     </div>
   }
 

flathub-infra/website

frontend/pages/wallet.tsx
@@ -6,6 +6,7 @@ import Router from 'next/router'
 import { ReactElement, useEffect } from 'react'
 import Main from '../src/components/layout/Main'
 import SavedCards from '../src/components/payment/cards/SavedCards'
+import TransactionHistory from '../src/components/payment/transactions/TransactionHistory'
 import Spinner from '../src/components/Spinner'
 import { useUserContext } from '../src/context/user-info'
 import styles from './userpage.module.scss'
@@ -30,6 +31,7 @@ export default function Wallet() {
     content = (
       <div className={styles.userArea}>
         <SavedCards />
+        <TransactionHistory />
       </div>
     )
   }