5b4d104e509218069a81f1d8997cb82415e411e9
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-04-20 14:53:33 +0100

    Add LoginGuard convenience wrapper component
    - An easy way to only load a page's content when we know the user is
      logged in. Otherwise redirecting them to login.

--- frontend/pages/wallet.tsx
+++ frontend/pages/wallet.tsx
@@ -2,44 +2,26 @@ import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
-import Router from 'next/router'
-import { ReactElement, useEffect } from 'react'
 import Main from '../src/components/layout/Main'
+import LoginGuard from '../src/components/login/LoginGuard'
 import SavedCards from '../src/components/payment/cards/SavedCards'
 import TransactionHistory from '../src/components/payment/transactions/TransactionHistory'
-import Spinner from '../src/components/Spinner'
-import { useUserContext } from '../src/context/user-info'
 import styles from './userpage.module.scss'
 
 // This is a proof of concept page, ideally this information will be
 // integrated into the user page as a tab or similar
 export default function Wallet() {
   const { t } = useTranslation()
-  const user = useUserContext()
-
-  // Nothing to show if not logged in, return to home
-  useEffect(() => {
-    if (!user.info && !user.loading) {
-      Router.push('/')
-    }
-  }, [user])
-
-  let content: ReactElement
-  if (user.loading || !user.info) {
-    content = <Spinner size={150} />
-  } else {
-    content = (
-      <div className={styles.userArea}>
-        <SavedCards />
-        <TransactionHistory />
-      </div>
-    )
-  }
 
   return (
     <Main>
       <NextSeo title={t('user-wallet')} noindex={true} />
-      {content}
+      <div className={styles.userArea}>
+        <LoginGuard>
+          <SavedCards />
+          <TransactionHistory />
+        </LoginGuard>
+      </div>
     </Main>
   )
 }
---
+++ frontend/src/components/login/LoginGuard.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router'
+import { FunctionComponent, useEffect } from 'react'
+import { useUserContext } from '../../context/user-info'
+import Spinner from '../Spinner'
+
+/**
+This is essentially a wrapper component to conditionally render the
+content within only once the user is confirmed to be logged in.
+
+If user is logged out, they will be redirected to login.
+
+TODO: Redirect back upon login completion
+*/
+const LoginGuard: FunctionComponent = ({ children }) => {
+  const user = useUserContext()
+  const router = useRouter()
+
+  // Content unsuitable if not logged in, send user to login page
+  useEffect(() => {
+    if (!user.info && !user.loading) {
+      router.push('/login')
+    }
+  }, [user, router])
+
+  return user.loading || !user.info ? <Spinner size={150} /> : <>{children}</>
+}
+
+export default LoginGuard
