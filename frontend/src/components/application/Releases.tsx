4815a7dbb7447f36855ff96ec571bdc83d224e5f
Kolja Lampe <razzeee@gmail.com>
2022-04-22 15:17:09 +0200

    Make changes collapsible if needed

--- frontend/src/components/application/Releases.tsx
+++ frontend/src/components/application/Releases.tsx
@@ -1,27 +1,53 @@
 import { formatDistance } from 'date-fns'
 import { useTranslation } from 'next-i18next'
-import { FunctionComponent } from 'react'
+import {
+  FunctionComponent,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import { getLocale } from '../../localize'
 
 import { Release } from '../../types/Appstream'
 import styles from './Releases.module.scss'
+import useCollapse from 'react-collapsed'
 
 interface Props {
-  releases: Release[]
+  latestRelease: Release | null
 }
 
-const Releases: FunctionComponent<Props> = ({ releases }) => {
+const Releases: FunctionComponent<Props> = ({ latestRelease }) => {
+  const collapsedHeight = 60
+  const [scrollHeight, setScrollHeight] = useState(0)
+  const ref = useRef(null)
+
+  useLayoutEffect(() => {
+    setScrollHeight(ref.current.scrollHeight)
+  }, [ref])
+
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse({
+    collapsedHeight: collapsedHeight,
+  })
   const { t, i18n } = useTranslation()
-  const latestRelease = releases ? releases[0] : null
+
+  const noChangelog = useMemo(
+    () => `<ul><li>${t('no-changelog-provided')}</li></ul>`,
+    [t]
+  )
 
   return (
     <>
-      {releases && releases.length > 0 && (
+      {latestRelease && (
         <div className={styles.releases}>
-          {latestRelease && (
+          <div className={styles.release}>
             <div className={styles.releaseDetails}>
               <header>
-                <h3>{t('changes-in-version', { "version-number": latestRelease.version })}</h3>
+                <h3>
+                  {t('changes-in-version', {
+                    'version-number': latestRelease.version,
+                  })}
+                </h3>
                 <div>
                   {latestRelease.timestamp &&
                     formatDistance(
@@ -32,12 +58,28 @@ const Releases: FunctionComponent<Props> = ({ releases }) => {
                 </div>
               </header>
               <div
+                {...getCollapseProps()}
+                className={`${styles.releaseContent} ${
+                  !isExpanded && scrollHeight > collapsedHeight
+                    ? styles.collapsed
+                    : ''
+                }`}
+                ref={ref}
                 dangerouslySetInnerHTML={{
-                  __html: latestRelease.description ?? t('no-changelog-provided'),
+                  __html: latestRelease.description ?? noChangelog,
                 }}
               />
             </div>
-          )}
+            {scrollHeight > collapsedHeight && (
+              <button className={styles.expandButton} {...getToggleProps()}>
+                {isExpanded ? (
+                  <span className={styles.expandText}>{t('less')}</span>
+                ) : (
+                  <span className={styles.expandText}>{t('more')}</span>
+                )}
+              </button>
+            )}
+          </div>
         </div>
       )}
     </>
