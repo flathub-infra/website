217462977ef42de2f0914b26b3bd5c77b45bca4e
Sorin Davidoi <sorin@sorin-davidoi.com>
2022-05-11 21:57:38 +0200

    feat(frontend): add referrer policy header
    From https://infosec.mozilla.org/guidelines/web_security#referrer-policy:
    
    > `strict-origin-when-cross-origin` send full referrer on same origin, URL without the path on foreign origin
    
    This will prevent accidentally leaking user information from the URL to 3rd parties (e.g. if we ever create a path like `/user/:id`, 3rd parties shouldn't get the id in the header, but only `https://beta.flathub.org`).

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -52,6 +52,10 @@ module.exports = (phase) => ({
             value: "nosniff",
           },
           {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
             key: "Content-Security-Policy",
             value:
               /**
