7c2bf7800b5d2f89fe0f0aad9ef51cc8cd48d555
Kolja Lampe <razzeee@gmail.com>
2022-05-24 12:07:27 +0200

    Add csf headers and more via next safe

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,9 @@
-const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 const { i18n } = require("./next-i18next.config")
+const nextSafe = require("next-safe")
 
-const CONTENT_SECURITY_POLICY = `
-  base-uri 'self';
-  default-src 'none';
-  form-action 'none';
-  script-src 'self' https://webstats.gnome.org;
-  style-src 'self' 'unsafe-inline' https://dl.flathub.org;
-  font-src 'self' https://dl.flathub.org;
-  connect-src 'self' https://flathub.org https://webstats.gnome.org;
-  img-src 'self' https://dl.flathub.org https://webstats.gnome.org https://avatars.githubusercontent.com https://gitlab.com https://gitlab.gnome.org https://lh3.googleusercontent.com https://secure.gravatar.com data:;
-  frame-ancestors 'none';
-`
-  .replace(/\s{2,}/g, " ")
-  .trim()
+const isDev = process.env.NODE_ENV !== "production"
 
-module.exports = (phase) => ({
+module.exports = () => ({
   i18n,
   images: {
     domains: [
@@ -45,39 +33,22 @@ module.exports = (phase) => ({
     return [
       {
         source: "/:path*",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "Deny",
+        headers: nextSafe({
+          isDev,
+          contentSecurityPolicy: {
+            "base-uri": `'self' ${process.env.NEXT_PUBLIC_SITE_BASE_URI}`,
+            "default-src": "none",
+            "form-action": "none",
+            "connect-src": `'self' ${process.env.NEXT_PUBLIC_API_BASE_URI} https://webstats.gnome.org;`,
+            "img-src":
+              "'self' https://dl.flathub.org https://webstats.gnome.org https://avatars.githubusercontent.com https://gitlab.com https://gitlab.gnome.org https://lh3.googleusercontent.com https://secure.gravatar.com data:;",
+            "script-src": "'self' https://webstats.gnome.org/matomo.js",
+            "style-src":
+              "'self' 'unsafe-inline' https://dl.flathub.org/repo/assets/Inter-3.19/",
+            "font-src": "'self' https://dl.flathub.org/repo/assets/",
+            "frame-ancestors": "none",
           },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              /**
-               * For testing adjustments use https://addons.mozilla.org/en-GB/firefox/addon/laboratory-by-mozilla/
-               * (which allows you to overwrite the Content Security Policy of a particular website).
-               *
-               * Do not add `unsafe-inline` to `script-src`, as we are using dangerouslySetInnerHTML in a few places,
-               * which makes us vulnerable to arbitrary code execution if we receive unsanitized data from the APIs.
-               *
-               * For the development environment we either need to maintain a separate CSP or disable it altogether.
-               * This is because it makes use of `eval` and other features that we don't want to allow in the production environment.
-               */
-              phase === PHASE_PRODUCTION_BUILD ? CONTENT_SECURITY_POLICY : "",
-          },
-        ],
+        }),
       },
     ]
   },
