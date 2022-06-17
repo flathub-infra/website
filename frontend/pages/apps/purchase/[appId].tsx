5aaff5eeea1eb9e287370aadbff8533ec044c64c
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-06-17 14:28:26 +0100

    feat(frontend): initalise app purchase controls
    - Use the developer settings to set the amount to the recommended
      payment value.
    - Render out the settings to inform the purhcaser of what the minimum
      and preferred payment values are.
    - Refactor the app purchase controls to more easily enable reuse of the
      split component next.

--- frontend/pages/apps/purchase/[appId].tsx
+++ frontend/pages/apps/purchase/[appId].tsx
@@ -1,17 +1,27 @@
 import { GetStaticPaths, GetStaticProps } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { NextSeo } from "next-seo"
+import * as AppVendingControls from "../../../src/components/application/AppVendingControls"
 import LoginGuard from "../../../src/components/login/LoginGuard"
-import PurchaseInput from "../../../src/components/payment/PurchaseInput"
-import { fetchAppstream } from "../../../src/fetchers"
+import { fetchAppstream, fetchVendingConfig } from "../../../src/fetchers"
 import { Appstream } from "../../../src/types/Appstream"
+import { VendingConfig } from "../../../src/types/Vending"
 
-export default function AppPurchasePage({ app }: { app: Appstream }) {
+export default function AppPurchasePage({
+  app,
+  vendingConfig,
+}: {
+  app: Appstream
+  vendingConfig: VendingConfig
+}) {
   return (
     <div className="max-w-11/12 my-0 mx-auto w-11/12 2xl:w-[1400px] 2xl:max-w-[1400px]">
       <NextSeo title={app?.name} />
       <LoginGuard>
-        <PurchaseInput appId={app.id} />
+        <AppVendingControls.PurchaseControls
+          app={app}
+          vendingConfig={vendingConfig}
+        />
       </LoginGuard>
     </div>
   )
@@ -21,13 +31,17 @@ export const getStaticProps: GetStaticProps = async ({
   locale,
   params: { appId },
 }) => {
-  const app = await fetchAppstream(appId as string)
+  const [app, vendingConfig] = await Promise.all([
+    fetchAppstream(appId as string),
+    fetchVendingConfig(),
+  ])
 
   return {
     notFound: !app,
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
       app,
+      vendingConfig,
     },
     revalidate: 3600,
   }
