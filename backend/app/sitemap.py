85f9cf60076e3eb464a8845bcd131419265a16a3
Razze <razzeee@gmail.com>
2022-06-19 11:56:24 +0200

    Add languages, categories and developers to sitemap

--- backend/app/sitemap.py
+++ backend/app/sitemap.py
@@ -11,7 +11,6 @@ def generate_text(frontend_url="beta.flathub.org"):
         "/about",
         "/apps",
         "/apps/collection/editors-choice-apps",
-        "/apps/collection/editors-choice-apps",
         "/apps/collection/editors-choice-games",
         "/apps/collection/popular",
         "/apps/collection/recently-updated",
@@ -20,9 +19,44 @@ def generate_text(frontend_url="beta.flathub.org"):
         "/privacy-policy",
         "/statistics",
         "/terms-and-conditions",
+        "/languages",
+    ]
+
+    languages = [
+        "en_GB",
+        "en",
+        "de",
+        "fr",
+        "nb_NO",
+        "tr",
+        "fi",
+        "id",
+        "it",
+        "pl",
+        "pt_BR",
+        "ru",
+        "si",
+        "vi",
+        "ar",
+        "es",
+        "ja",
+        "cs",
+        "zh_Hans",
+        "bg",
+        "uk",
+        "et",
+        "ca",
+        "el",
     ]
 
     applications = [f"/apps/details/{appid}" for appid in apps.list_appstream()]
+
+    categories = [f"/apps/category/{category}" for category in db.get_categories()]
+
+    developers = [
+        f"/apps/collection/developer/{developer}" for developer in db.get_developers()
+    ]
+
     projectgroups = [
         f"/apps/collection/project-group/{projectgroup}"
         for projectgroup in db.get_project_groups()
@@ -31,8 +65,21 @@ def generate_text(frontend_url="beta.flathub.org"):
     sitemap = "\n".join(
         sorted(
             f"https://{frontend_url}{page}"
-            for page in pages + applications + projectgroups
+            for page in pages + applications + projectgroups + developers + categories
         )
     )
+
+    for language in languages:
+        sitemap += "\n".join(
+            sorted(
+                f"https://{frontend_url}/{language}{page}"
+                for page in pages
+                + applications
+                + projectgroups
+                + developers
+                + categories
+            )
+        )
+
     db.redis_conn.setex(redis_key, 60 * 60 * 24, sitemap)
     return sitemap
