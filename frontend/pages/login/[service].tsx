a20a35e660622d9cbc5141679bd55a76ad3b14ef
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-06-23 17:41:10 +0100

    fix(frontend): redirect to stored page on login
    The effect was running a second time when the stored value was
    cleared, which results in default redirection to the user page.
    
    I also updated the page to use the async hook and break the code up for
    clairty. Could probably benefit from breaking logic out to components at
    some point.

--- frontend/pages/login/[service].tsx
+++ frontend/pages/login/[service].tsx
@@ -3,35 +3,59 @@ import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { NextSeo } from "next-seo"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { toast } from "react-toastify"
 import { login } from "../../src/asyncs/login"
 import Spinner from "../../src/components/Spinner"
 import { useUserContext, useUserDispatch } from "../../src/context/user-info"
 import { fetchLoginProviders } from "../../src/fetchers"
+import { useAsync } from "../../src/hooks/useAsync"
 import { useLocalStorage } from "../../src/hooks/useLocalStorage"
 import { usePendingTransaction } from "../../src/hooks/usePendingTransaction"
 import { isInternalRedirect } from "../../src/utils/security"
 
-export default function AuthReturnPage({ services }) {
+export default function AuthReturnPage({ services }: { services: string[] }) {
   // Must access query params to POST to backend for oauth verification
   const { t } = useTranslation()
   const router = useRouter()
 
-  // Send only one request, prevent infinite loops
-  const [sent, setSent] = useState(false)
-
   const user = useUserContext()
   const dispatch = useUserDispatch()
 
-  const [pendingTransaction, _setPendingTransaction] = usePendingTransaction()
+  const [pendingTransaction] = usePendingTransaction()
   const [returnTo, setReturnTo] = useLocalStorage<string>("returnTo", "")
 
+  const { execute, status, error } = useAsync(
+    useCallback(() => login(dispatch, router.query), [dispatch, router.query]),
+    false,
+  )
+
   useEffect(() => {
+    if (error) {
+      toast.error(t(error))
+    }
+  }, [t, error])
+
+  // Once router ready, perform login
+  useEffect(() => {
+    if (router.isReady) {
+      execute()
+    }
+  }, [router.isReady, execute])
+
+  // Prevent mistaken ridirection after stored value is cleared
+  const [redirected, setRedirected] = useState(false)
+
+  useEffect(() => {
+    if (redirected) return
+
     // Redirect back to where user was, or userpage for unprompted login
-    if (user.info && !user.loading) {
+    if (status === "success" && user.info && !user.loading) {
+      setRedirected(true)
+
       if (pendingTransaction) {
         router.push("/purchase")
+        return
       } else if (returnTo) {
         const redirect = decodeURIComponent(returnTo)
         setReturnTo(null)
@@ -39,20 +63,26 @@ export default function AuthReturnPage({ services }) {
         // Must validate the redirect to prevent open redirect and code execution
         if (isInternalRedirect(redirect)) {
           router.push(redirect)
-        } else {
-          router.push("/userpage")
+          return
         }
-      } else {
-        router.push("/userpage")
       }
-    }
 
-    // Router must be ready to access query parameters
-    if (!router.isReady) {
-      return
+      router.push("/userpage")
     }
+  }, [
+    status,
+    user,
+    pendingTransaction,
+    returnTo,
+    router,
+    redirected,
+    setReturnTo,
+  ])
 
-    // Redirect away if user tries some kind of directory traversal
+  // Redirect away if user tries some kind of directory traversal
+  useEffect(() => {
+    if (!router.isReady) return
+
     if (
       services.every((s: string) => s !== router.query.service) ||
       router.query.code == null ||
@@ -61,27 +91,13 @@ export default function AuthReturnPage({ services }) {
       router.push(user.info ? "/userpage" : "/")
       return
     }
+  }, [router, user, services])
 
-    if (!sent) {
-      setSent(true)
-      login(dispatch, router.query).catch((err) => toast.error(t(err)))
-    }
-  }, [
-    router,
-    dispatch,
-    user,
-    sent,
-    services,
-    t,
-    pendingTransaction,
-    returnTo,
-    setReturnTo,
-  ])
-
+  // This is purely a loading page
   return (
     <>
       <NextSeo title={t("login")} noindex={true}></NextSeo>
-      {user.loading ? <Spinner size="l" /> : <></>}
+      <Spinner size="l" />
     </>
   )
 }
