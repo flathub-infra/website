279c13879f5a4918f1a8d8ccecdf70706f8cbb9a
Kyle Mckay <kyle.mckay@codethink.co.uk>
2022-07-06 11:45:01 +0100

    chore: cleanup old code and comments

--- frontend/src/types/Input.ts
+++ frontend/src/types/Input.ts
@@ -2,7 +2,7 @@
  * For best UX practice numeric values are input as text.
  *
  * Both the value during editing and the resolved number on blur need to be
- * state shared elsewhere in the form.
+ * state shared elsewhere in the form for constraint and feedback.
  */
 export interface NumericInputValue {
   live: number
--- frontend/src/utils/charts.ts
+++ frontend/src/utils/charts.ts
@@ -1,30 +1,12 @@
 import { ChartData, ChartDataset } from "chart.js"
 
-export function doughnutData(
-  labels: (string | Date)[],
-  data: number[],
-  mode: "dark" | "light",
-): ChartData<"doughnut", number[]> {
-  const lightness = mode === "light" ? "55.1%" : "calc(55.1% - 15%)"
-
-  const colors = []
-  for (let index = 0; index < data.length; index++) {
-    colors.push(`hsl(${212.9 - index * 35}, 58.1%, ${lightness})`)
-  }
-
-  return {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: colors,
-        borderColor: "white",
-        borderJoinStyle: "miter",
-        data: data,
-      },
-    ],
-  }
-}
-
+/**
+ * Constructs the chart data for a single stacked bar.
+ * @param labels list of bar segment labels
+ * @param data list of bar segment values
+ * @param mode theme to color chart
+ * @returns bar chart data object
+ */
 export function stackedBarData(
   labels: (string | Date)[],
   data: number[],
