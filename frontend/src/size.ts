f31e769681b4ce32169ad60bd18ae8711ab22c73
JakobDev <jakobdev@gmx.de>
2022-07-07 08:32:04 +0200

    Support GB in calculateHumanReadableSize()

--- frontend/src/size.ts
+++ frontend/src/size.ts
@@ -1,5 +1,7 @@
 export function calculateHumanReadableSize(size: number) {
-  if (size > 1_000_000) {
+  if (size > 1_000_000_000) {
+    return (size / 1_000_000_000).toFixed(2) + " GB"
+  } else if (size > 1_000_000) {
     return Math.round(size / 1_000_000) + " MB"
   } else {
     return Math.round(size / 1_000) + " KB"
