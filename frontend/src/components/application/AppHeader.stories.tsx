ccde8745d93921a2838f40a84e0ccc74bff76239
Razze <razzeee@gmail.com>
2022-08-07 20:27:49 +0200

    Split app header

---
+++ frontend/src/components/application/AppHeader.stories.tsx
@@ -0,0 +1,124 @@
+import React from "react"
+import { ComponentMeta } from "@storybook/react"
+import { faker } from "@faker-js/faker"
+import { AppHeader } from "./AppHeader"
+import { Appstream } from "src/types/Appstream"
+import { VendingSetup } from "src/types/Vending"
+
+export default {
+  title: "Components/Application/AppHeader",
+  component: AppHeader,
+} as ComponentMeta<typeof AppHeader>
+
+export const Install = () => {
+  const app = {
+    id: faker.datatype.uuid(),
+    icon: faker.image.image(),
+    name: faker.commerce.product(),
+    developer_name: faker.internet.userName(),
+  }
+
+  const user = {
+    loading: false,
+    info: {
+      "dev-flatpaks": [],
+    },
+  }
+
+  return (
+    <AppHeader
+      app={app}
+      user={user}
+      installClicked={() => {}}
+      donateClicked={() => {}}
+      vendingSetup={undefined}
+    />
+  )
+}
+
+export const InstallWithDonate = () => {
+  const app: Appstream = {
+    id: faker.datatype.uuid(),
+    icon: faker.image.image(),
+    name: faker.commerce.product(),
+    developer_name: faker.internet.userName(),
+    urls: { donation: faker.internet.url() },
+  }
+
+  const user = {
+    loading: false,
+    info: {
+      "dev-flatpaks": [],
+    },
+  }
+
+  return (
+    <AppHeader
+      app={app}
+      user={user}
+      installClicked={() => {}}
+      donateClicked={() => {}}
+      vendingSetup={undefined}
+    />
+  )
+}
+
+export const UserOwnsApp = () => {
+  const appId = faker.datatype.uuid()
+  const app: Appstream = {
+    id: appId,
+    icon: faker.image.image(),
+    name: faker.commerce.product(),
+    developer_name: faker.internet.userName(),
+    urls: { donation: faker.internet.url() },
+  }
+
+  const user = {
+    loading: false,
+    info: {
+      "dev-flatpaks": [appId],
+    },
+  }
+
+  return (
+    <AppHeader
+      app={app}
+      user={user}
+      installClicked={() => {}}
+      donateClicked={() => {}}
+      vendingSetup={undefined}
+    />
+  )
+}
+
+export const WithVending = () => {
+  const appId = faker.datatype.uuid()
+  const app: Appstream = {
+    id: appId,
+    icon: faker.image.image(),
+    name: faker.commerce.product(),
+    developer_name: faker.internet.userName(),
+    urls: { donation: faker.internet.url() },
+  }
+
+  const user = {
+    loading: false,
+    info: {
+      "dev-flatpaks": [appId],
+    },
+  }
+
+  const vending: VendingSetup = {
+    recommended_donation: faker.commerce.price(),
+  }
+
+  return (
+    <AppHeader
+      app={app}
+      user={user}
+      installClicked={() => {}}
+      donateClicked={() => {}}
+      vendingSetup={vending}
+    />
+  )
+}
---
+++ frontend/src/components/application/AppHeader.tsx
@@ -0,0 +1,79 @@
+import { useTranslation } from "next-i18next"
+import Link from "next/link"
+import React from "react"
+import { Appstream } from "src/types/Appstream"
+import { UserState } from "src/types/Login"
+import { VendingSetup } from "src/types/Vending"
+import Button from "../Button"
+import LogoImage from "../LogoImage"
+
+export function AppHeader({
+  app,
+  user,
+  installClicked,
+  donateClicked,
+  vendingSetup,
+}: {
+  app: Appstream
+  user: UserState
+  installClicked: (e: any) => void
+  donateClicked: (e: any) => void
+  vendingSetup: VendingSetup
+}) {
+  const { t } = useTranslation()
+
+  return (
+    <header className="col-start-2 flex w-full py-7">
+      {app.icon && (
+        <div className="m-2 flex max-h-[128px] max-w-[128px] self-center drop-shadow-md">
+          <LogoImage iconUrl={app.icon} appName={app.name} />
+        </div>
+      )}
+
+      <div className="mx-3 my-auto">
+        <h2 className="mt-0 mb-3">{app.name}</h2>
+        {app.developer_name?.trim().length > 0 && (
+          <div className="text-sm font-light">
+            {t("by", {
+              developer: app.developer_name,
+            })}
+          </div>
+        )}
+      </div>
+
+      <div className="ml-auto">
+        {!user.loading && user.info?.["dev-flatpaks"].includes(app.id) && (
+          <Link passHref href={`/apps/manage/${app.id}`}>
+            <Button className="mb-3 block w-full last:mb-0">
+              {t("developer-settings")}
+            </Button>
+          </Link>
+        )}
+        <Button
+          className="mb-3 block w-full last:mb-0"
+          onClick={installClicked}
+        >
+          {t("install")}
+        </Button>
+        {app.urls?.donation && (
+          <a
+            href={app.urls.donation}
+            target="_blank"
+            rel="noreferrer"
+            onClick={donateClicked}
+            className="mb-3 block w-full no-underline last:mb-0"
+          >
+            <Button variant="secondary">{t("donate")}</Button>
+          </a>
+        )}
+        {!!vendingSetup?.recommended_donation && (
+          <Link passHref href={`/apps/purchase/${app.id}`}>
+            <Button className="mb-3 block w-full last:mb-0">
+              {t("kind-purchase")}
+            </Button>
+          </Link>
+        )}
+      </div>
+    </header>
+  )
+}
