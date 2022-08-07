d00bb611aed9a43220a7bac27f3dc7aa24c60d3b
Razze <razzeee@gmail.com>
2022-08-07 20:07:32 +0200

    Standardize blue
    Closes #501

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -17,18 +17,18 @@ const Button: FunctionComponent<Props> = forwardRef<HTMLButtonElement, Props>(
   ({ children, variant = "primary", className, ...buttonProps }, ref) => {
     const variantClass = {
       destructive:
-        "bg-bgColorSecondary text-colorDanger border border-colorDanger disabled:borden-none disabled:text-gray-100 hover:bg-colorDanger hover:text-gray-100",
+        "bg-bgColorSecondary text-colorDanger border border-colorDanger disabled:borden-none disabled:text-gray-100 enabled:hover:bg-colorDanger enabled:hover:text-gray-100",
       secondary:
-        "bg-bgColorSecondary text-colorSecondary border border-colorSecondary disabled:border-none disabled:text-gray-100 disabled:bg-gray-500 hover:bg-colorPrimary hover:text-gray-100",
+        "bg-bgColorSecondary text-textSecondary border border-textSecondary disabled:border-none disabled:text-gray-100 disabled:bg-gray-500 enabled:hover:opacity-50",
       primary:
-        "bg-colorPrimary text-gray-100 disabled:bg-gray-400 hover:bg-colorSecondary",
+        "bg-colorPrimary text-gray-100 disabled:bg-gray-400 enabled:hover:opacity-75",
     }[variant]
 
     return (
       <button
         className={`${
           className ?? ""
-        } ${variantClass} no-wrap h-11 overflow-hidden text-ellipsis whitespace-nowrap px-5 py-2 text-center duration-500 hover:cursor-pointer active:bg-bgColorPrimary active:text-colorPrimary disabled:cursor-default`}
+        } ${variantClass} no-wrap h-11 overflow-hidden text-ellipsis whitespace-nowrap px-5 py-2 text-center duration-500 active:bg-bgColorPrimary active:text-colorPrimary enabled:hover:cursor-pointer disabled:cursor-default`}
         type={buttonProps.type}
         ref={ref}
         {...buttonProps}
---
+++ frontend/src/components/login/ProviderLink.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { ComponentMeta } from "@storybook/react"
+import ProviderLink from "./ProviderLink"
+import { LoginProvider } from "src/types/Login"
+
+export default {
+  title: "Components/Login/ProviderLink",
+  component: ProviderLink,
+} as ComponentMeta<typeof ProviderLink>
+
+export const github = () => {
+  const provider: LoginProvider = {
+    method: "github",
+    name: "GitHub",
+  }
+
+  return <ProviderLink provider={provider} />
+}
+
+export const gitlab = () => {
+  const provider: LoginProvider = {
+    method: "gitlab",
+    name: "GitLab",
+  }
+
+  return <ProviderLink provider={provider} />
+}
+
+export const gnome = () => {
+  const provider: LoginProvider = {
+    method: "gnome",
+    name: "GNOME",
+  }
+
+  return <ProviderLink provider={provider} />
+}
+
+export const google = () => {
+  const provider: LoginProvider = {
+    method: "google",
+    name: "Google",
+  }
+
+  return <ProviderLink provider={provider} />
+}
