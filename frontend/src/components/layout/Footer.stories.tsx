a774c11a2553aca2620309d061bbd598d25e39e0
Razze <razzeee@gmail.com>
2022-08-12 11:34:06 +0200

    Add header and footer stories

---
+++ frontend/src/components/layout/Footer.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ComponentMeta } from "@storybook/react"
+import Footer from "./Footer"
+
+export default {
+  title: "Components/Layout/Footer",
+  component: Footer,
+} as ComponentMeta<typeof Footer>
+
+export const Generated = () => {
+  return <Footer />
+}
