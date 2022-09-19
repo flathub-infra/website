4d52317b8ca7c236f085b2d33f9596dfa265ce1f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-19 09:35:22 +0200

    Add endpoint for serving linting exceptions data
    This will let us decouple exceptions list from the linting tool.

---
+++ backend/app/exceptions.py
@@ -0,0 +1,17 @@
+import json
+
+import requests
+
+from . import db
+
+
+def update():
+    r = requests.get(
+        "https://raw.githubusercontent.com/barthalion/flatpak-builder-lint/master/flatpak_builder_lint/exceptions.json"
+    )
+
+    if r.status_code == 200:
+        exceptions = r.json()
+        db.redis_conn.mset(
+            {f"exc:{appid}": json.dumps(exceptions[appid]) for appid in exceptions}
+        )
