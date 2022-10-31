e6d510e30899298b0784f59b8c7c07ade74b80d4
Razze <razzeee@gmail.com>
2022-10-31 14:23:21 +0100

    Markup breadcrumbs for seo

--- frontend/src/components/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "next-i18next"
+import { BreadcrumbJsonLd } from "next-seo"
 import Link from "next/link"
 import { FunctionComponent } from "react"
 import { HiChevronRight, HiHome } from "react-icons/hi2"
@@ -12,36 +13,45 @@ const Breadcrumbs: FunctionComponent<Props> = ({ pages }) => {
   const { t } = useTranslation()
 
   return (
-    <nav className="flex" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-4">
-        <li>
-          <div>
-            <a href="" className="text-zinc-400 hover:text-gray-500">
-              <HiHome className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-              <span className="sr-only">{t("home")}</span>
-            </a>
-          </div>
-        </li>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className="flex items-center">
-              <HiChevronRight
-                className="h-5 w-5 flex-shrink-0 text-zinc-400"
-                aria-hidden="true"
-              />
-              <Link href={page.href} passHref>
-                <a
-                  className="ml-4 text-sm font-medium text-zinc-400 no-underline hover:text-zinc-500"
-                  aria-current={page.current ? "page" : undefined}
-                >
-                  {t(page.name)}
-                </a>
-              </Link>
+    <>
+      <BreadcrumbJsonLd
+        itemListElements={pages.map((page) => ({
+          position: pages.indexOf(page) + 1,
+          name: t(page.name),
+          item: page.href,
+        }))}
+      />
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol role="list" className="flex items-center space-x-4">
+          <li>
+            <div>
+              <a href="" className="text-zinc-400 hover:text-gray-500">
+                <HiHome className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+                <span className="sr-only">{t("home")}</span>
+              </a>
             </div>
           </li>
-        ))}
-      </ol>
-    </nav>
+          {pages.map((page) => (
+            <li key={page.name}>
+              <div className="flex items-center">
+                <HiChevronRight
+                  className="h-5 w-5 flex-shrink-0 text-zinc-400"
+                  aria-hidden="true"
+                />
+                <Link href={page.href} passHref>
+                  <a
+                    className="ml-4 text-sm font-medium text-zinc-400 no-underline hover:text-zinc-500"
+                    aria-current={page.current ? "page" : undefined}
+                  >
+                    {t(page.name)}
+                  </a>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </nav>
+    </>
   )
 }
 
