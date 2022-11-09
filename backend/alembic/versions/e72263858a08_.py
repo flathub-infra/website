aabc30c37af0c7f2a0454433cdf0c874bbfb1d1a
James Westman <james@jwestman.net>
2022-11-09 11:19:41 -0600

    Merge alembic revisions

---
+++ backend/alembic/versions/e72263858a08_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e72263858a08
+Revises: 76709cd2131b, 4daddefa74c5
+Create Date: 2022-11-09 11:19:08.629222
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e72263858a08'
+down_revision = ('76709cd2131b', '4daddefa74c5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
