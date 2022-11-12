4312d6470719dfe9c215d04d32d35d7a247c52ae
Razze <razzeee@gmail.com>
2022-11-13 00:34:26 +0100

    Show verification in richer popup, that should also work on mobile
    Closes #781

--- frontend/src/components/application/Verification.tsx
+++ frontend/src/components/application/Verification.tsx
@@ -1,9 +1,18 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useRef, useState } from "react"
 import React from "react"
 
 import { HiCheckBadge } from "react-icons/hi2"
 import { useTranslation } from "next-i18next"
 import { VerificationStatus } from "src/types/VerificationStatus"
+import {
+  useFloating,
+  useHover,
+  useInteractions,
+  arrow,
+  offset,
+  shift,
+  autoPlacement,
+} from "@floating-ui/react-dom-interactions"
 
 interface Props {
   verificationStatus: VerificationStatus
@@ -12,26 +21,86 @@ interface Props {
 const Verification: FunctionComponent<Props> = ({ verificationStatus }) => {
   const { t } = useTranslation()
 
+  const arrowRef = useRef(null)
+  const [open, setOpen] = useState(false)
+  const {
+    x,
+    y,
+    reference,
+    floating,
+    strategy,
+    context,
+    middlewareData: { arrow: { x: arrowX, y: arrowY } = {} },
+    placement,
+  } = useFloating({
+    open,
+    onOpenChange: setOpen,
+    middleware: [
+      shift(),
+      autoPlacement(),
+      offset(6),
+      arrow({ element: arrowRef }),
+    ],
+  })
+  const hover = useHover(context, {})
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover])
+
+  const staticSide = {
+    top: "bottom",
+    right: "left",
+    bottom: "top",
+    left: "right",
+  }[placement.split("-")[0]]
+
   if (verificationStatus?.verified == true) {
     return (
-      <HiCheckBadge
-        className="h-10 w-10 text-colorPrimary"
-        title={
-          verificationStatus.method == "manual"
-            ? t("verified-manually")
-            : verificationStatus.method == "website"
-            ? t("verified-website", {
-                website: verificationStatus.website,
-              })
-            : verificationStatus.method == "login_provider"
-            ? t("verified-login-provider", {
-                login_provider: verificationStatus.login_provider,
-                login_name: verificationStatus.login_name,
-              })
-            : t("verified") // Should never happen
-        }
-        aria-label={t("app-is-verified")}
-      />
+      <>
+        <button ref={reference} {...getReferenceProps}>
+          <HiCheckBadge
+            className="h-10 w-10 text-colorPrimary"
+            aria-label={t("app-is-verified")}
+          />
+        </button>
+        {open && (
+          <div
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
+            }}
+            className="w-max rounded-xl bg-bgColorSecondary p-4"
+            {...getFloatingProps()}
+          >
+            {
+              verificationStatus.method == "manual"
+                ? t("verified-manually")
+                : verificationStatus.method == "website"
+                ? t("verified-website", {
+                    website: verificationStatus.website,
+                  })
+                : verificationStatus.method == "login_provider"
+                ? t("verified-login-provider", {
+                    login_provider: verificationStatus.login_provider,
+                    login_name: verificationStatus.login_name,
+                  })
+                : t("verified") // Should never happen
+            }
+            <div
+              ref={arrowRef}
+              className="absolute h-4 w-4 rotate-45 bg-bgColorSecondary"
+              style={{
+                top: arrowY != null ? `${arrowY}px` : "",
+                left: arrowX != null ? `${arrowX}px` : "",
+                right: "",
+                bottom: "",
+                [staticSide]: "-4px",
+              }}
+            />
+          </div>
+        )}
+      </>
     )
   } else {
     return null
