ead48f77225a601a6a8da1bfa8b4bcf84be45812
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-11-30 12:43:08 +0100

    Drop leftover comment

--- backend/tests/run_tests.sh
+++ backend/tests/run_tests.sh
@@ -1,15 +1,6 @@
 #/bin/bash
 set -x
 
-# for i, test_stats_json in enumerate(
-#         sorted(glob.glob("tests/stats/*.json"), reverse=True)
-#     ):
-#         date = datetime.date.today() - datetime.timedelta(days=i)
-#         stats_file = os.path.join(workspace.name, date.strftime("%Y/%m/%d.json"))
-#         os.makedirs(os.path.dirname(stats_file), exist_ok=True)
-#         print(f"Copy {test_stats_json} to {stats_file}")
-#         shutil.copy(test_stats_json, stats_file)
-
 today="$(date -d "today" +"%Y/%m/%d.json")"
 yesterday="$(date -d "yesterday" +"%Y/%m/%d.json")"
 two_days_ago="$(date -d "2 days ago" +"%Y/%m/%d.json")"
