06b66643f51e4a3bde4a9f269df041f560dd8198
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-01-24 22:46:14 +0100

    Use @next/font to serve the Inter font

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -7,10 +7,6 @@ export default function Document() {
         <base href="/" />
 
         <link rel="icon" type="image/png" href="./favicon.png" />
-        <link
-          href="https://dl.flathub.org/repo/assets/Inter-3.19/inter.css"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
