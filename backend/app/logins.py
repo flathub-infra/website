6f8060f507ff81d8911933898f89550ef5c44589
Razze <razzeee@gmail.com>
2023-02-08 16:54:37 +0100

    Sort owned flatpaks too

--- backend/app/logins.py
+++ backend/app/logins.py
@@ -771,7 +771,7 @@ def get_userinfo(login=Depends(login_state)):
     }
 
     ret["dev-flatpaks"] = sorted(ret["dev-flatpaks"].union(dev_flatpaks))
-    ret["owned-flatpaks"] = owned_flatpaks
+    ret["owned-flatpaks"] = sorted(owned_flatpaks)
 
     gha = models.GithubAccount.by_user(db, user)
     if gha is not None:
