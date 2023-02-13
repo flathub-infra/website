f1f14b8b8a76bdf31f6f47f851a5b748ec8396ba
Razze <razzeee@gmail.com>
2023-02-13 23:59:23 +0100

    Improve error pages
    Closes #1050

--- frontend/pages/500.tsx
+++ frontend/pages/500.tsx
@@ -10,7 +10,7 @@ export default function Custom500() {
       <NextSeo title={t("server-error")} />
       <div className="max-w-11/12 my-0 mx-auto w-11/12 2xl:w-[1400px] 2xl:max-w-[1400px]">
         <h1>{t("whoops")}</h1>
-        <p>{t("an-error-occurred-server")}</p>
+        <p>{t("an-error-occurred-server", { errorCode: "500" })}</p>
         <p>
           <Trans i18nKey={"common:retry-or-go-home"}>
             You might want to retry or go back <a href=".">home</a>.
