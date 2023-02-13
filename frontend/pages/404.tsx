2c9b407653c4bb686e55891600fa98e545054b80
Razze <razzeee@gmail.com>
2023-02-13 23:59:23 +0100

    Improve error pages
    Closes #1050

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -7,10 +7,10 @@ export default function Custom404() {
   const { t } = useTranslation()
   return (
     <>
-      <NextSeo title={t("client-error")} />
+      <NextSeo title={t("page-not-found", { errorCode: "404" })} />
       <div className="max-w-11/12 my-0 mx-auto w-11/12 2xl:w-[1400px] 2xl:max-w-[1400px]">
-        <h1>{t("whoops")}</h1>
-        <p>{t("an-error-occurred-client")}</p>
+        <h1>{t("page-not-found", { errorCode: "404" })}</h1>
+        <p>{t("could-not-find-page")}</p>
         <p>
           <Trans i18nKey={"common:retry-or-go-home"}>
             You might want to retry or go back <a href=".">home</a>.
