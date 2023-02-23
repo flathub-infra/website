95d0270f1e3ca1c4fd01acecfc61fc3c14624890
Razze <razzeee@gmail.com>
2023-02-23 01:51:38 +0100

    Improve app card hover styles
    Closes #1106

--- frontend/src/components/application/ApplicationCard.tsx
+++ frontend/src/components/application/ApplicationCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import LogoImage from "../LogoImage"
 
 import { AppstreamListItem } from "../../types/Appstream"
+import { classNames } from "src/styling"
 
 interface Props {
   application: AppstreamListItem
@@ -12,7 +13,11 @@ const ApplicationCard: FunctionComponent<Props> = ({ application }) => (
   <Link
     href={`/apps/${application.id}`}
     passHref
-    className="flex min-w-0 items-center gap-4 rounded-xl bg-flathub-white p-4 shadow-md duration-500 hover:cursor-pointer hover:no-underline hover:shadow-xl hover:brightness-95 active:bg-flathub-gray-98 dark:bg-flathub-jet active:dark:bg-flathub-raisin-black"
+    className={classNames(
+      "flex min-w-0 items-center gap-4 rounded-xl bg-flathub-white p-4 shadow-md duration-500 dark:bg-flathub-jet",
+      "hover:cursor-pointer hover:bg-flathub-gray-98 hover:no-underline hover:shadow-xl dark:hover:bg-flathub-outer-space",
+      "active:bg-flathub-gray-92 active:dark:bg-flathub-gunmetal",
+    )}
   >
     <div className="relative h-[64px] w-[64px] flex-shrink-0 flex-wrap items-center justify-center drop-shadow-md md:h-[128px] md:w-[128px]">
       <LogoImage iconUrl={application.icon} appName={application.name} />
