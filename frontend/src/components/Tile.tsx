8d7eb953a206fc667f350b84874e79c25a3ae9a4
Kolja Lampe <razzeee@gmail.com>
2023-03-01 16:11:54 +0100

    Use same tile hover/active style the app cards use

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent, forwardRef } from "react"
+import { classNames } from "src/styling"
 
 interface Props {
   children: React.ReactNode
@@ -11,7 +12,13 @@ const Tile: FunctionComponent<Props> = forwardRef<HTMLAnchorElement, Props>(
   ({ children, className, onClick, href }, ref) => {
     return (
       <a
-        className={`flex items-center justify-center break-words rounded-xl bg-flathub-white p-3 text-center text-flathub-sonic-silver shadow-md duration-500 hover:bg-flathub-gainsborow dark:bg-flathub-arsenic dark:text-flathub-spanish-gray hover:dark:bg-flathub-dark-gunmetal  ${className}`}
+        className={classNames(
+          "flex items-center justify-center break-words rounded-xl p-3 text-center duration-500",
+          "bg-flathub-white text-flathub-sonic-silver shadow-md dark:bg-flathub-arsenic dark:text-flathub-spanish-gray",
+          "hover:bg-flathub-gainsborow/5 hover:no-underline hover:shadow-xl dark:hover:bg-flathub-granite-gray",
+          "active:bg-flathub-gray-x11 active:dark:bg-flathub-dark-gunmetal",
+          className,
+        )}
         href={href}
         onClick={onClick}
         ref={ref}
