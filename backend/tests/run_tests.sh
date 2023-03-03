10cd01c85dfaf37b390cc3b4eb0c7c2a2a56850b
Kolja Lampe <razzeee@gmail.com>
2023-03-03 02:05:59 +0100

    Fix rm command and install file

--- backend/tests/run_tests.sh
+++ backend/tests/run_tests.sh
@@ -5,9 +5,10 @@ today="$(date -d "today" +"%Y/%m/%d.json")"
 yesterday="$(date -d "yesterday" +"%Y/%m/%d.json")"
 two_days_ago="$(date -d "2 days ago" +"%Y/%m/%d.json")"
 
-rm -rf tests/stats/20.*
+rm -rf tests/stats/20*
 install -d tests/stats/$(dirname $today)
 install -d tests/stats/$(dirname $yesterday)
+install -d tests/stats/$(dirname $two_days_ago)
 cp tests/stats/001-day-before-yesterday.json tests/stats/$two_days_ago
 cp tests/stats/002-yesterday.json tests/stats/$yesterday
 cp tests/stats/003-today.json tests/stats/$today
