4b23a5c69e208d30a74d598c004585544efcb5fb
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-03-30 17:39:27 +0200

    logins: Add team-writable repositories to the repos list

--- backend/app/logins.py
+++ backend/app/logins.py
@@ -65,11 +65,23 @@ class UserDeleteRequest(BaseModel):
 def refresh_repo_list(gh_access_token: str, account: models.GithubAccount):
     gh = Github(gh_access_token)
     ghuser = gh.get_user()
-    repos = [
+    user_repos = [
         repo.full_name.removeprefix("flathub/")
         for repo in ghuser.get_repos(affiliation="collaborator")
         if repo.full_name.startswith("flathub/") and repo.permissions.push
     ]
+
+    gh_teams = [
+        team for team in ghuser.get_teams() if team.organization.login == "flathub"
+    ]
+    gh_team_repos = [
+        repo.full_name.removeprefix("flathub/")
+        for team in gh_teams
+        for repo in team.get_repos()
+        if repo.permissions.push
+    ]
+
+    repos = list(set(user_repos + gh_team_repos))
     models.GithubRepository.unify_repolist(db, account, repos)
 
 
