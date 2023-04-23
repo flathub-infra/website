115d6922b2207eb2e2883213ed6ea569db904db8
Razze <razzeee@gmail.com>
2023-04-23 12:59:52 +0200

    Don't let a section have paddings, let surrounding code decide those

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({
   return (
     <>
       <NextSeo title={t("home")} description={t("flathub-description")} />
-      <div className="max-w-11/12 mx-auto my-0 w-11/12 2xl:w-[1400px] 2xl:max-w-[1400px]">
+      <div className="max-w-11/12 mx-auto my-0 w-11/12 space-y-10 2xl:w-[1400px] 2xl:max-w-[1400px]">
         <div className="flex justify-between gap-3">
           <div className="prose dark:prose-invert">
             <h1 className="my-8">{t("apps-for-linux-right-here")}</h1>
