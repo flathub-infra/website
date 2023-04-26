ef584eb721e544a1df9d7b02b386dab573a3a563
Kolja Lampe <razzeee@gmail.com>
2023-04-26 10:23:35 +0200

    Change svg to react component correctly

--- frontend/src/components/login/GnomeLogo.tsx
+++ frontend/src/components/login/GnomeLogo.tsx
@@ -18,34 +18,34 @@ export const GnomeLogo = () => (
           fill="currentcolor"
           id="g3771"
         >
-          <g fill-opacity="1" fill="currentcolor" id="g3773">
+          <g fillOpacity="1" fill="currentcolor" id="g3773">
             <path
               d="M 86.068,0 C 61.466,0 56.851,35.041 70.691,35.041 C 84.529,35.041 110.671,0 86.068,0 z "
-              fill-opacity="1"
+              fillOpacity="1"
               fill="currentcolor"
               id="path3775"
             />
             <path
               d="M 45.217,30.699 C 52.586,31.149 60.671,2.577 46.821,4.374 C 32.976,6.171 37.845,30.249 45.217,30.699 z "
-              fill-opacity="1"
+              fillOpacity="1"
               fill="currentcolor"
               id="path3777"
             />
             <path
               d="M 11.445,48.453 C 16.686,46.146 12.12,23.581 3.208,29.735 C -5.7,35.89 6.204,50.759 11.445,48.453 z "
-              fill-opacity="1"
+              fillOpacity="1"
               fill="currentcolor"
               id="path3779"
             />
             <path
               d="M 26.212,36.642 C 32.451,35.37 32.793,9.778 21.667,14.369 C 10.539,18.961 19.978,37.916 26.212,36.642 L 26.212,36.642 z "
-              fill-opacity="1"
+              fillOpacity="1"
               fill="currentcolor"
               id="path3781"
             />
             <path
               d="M 58.791,93.913 C 59.898,102.367 52.589,106.542 45.431,101.092 C 22.644,83.743 83.16,75.088 79.171,51.386 C 75.86,31.712 15.495,37.769 8.621,68.553 C 3.968,89.374 27.774,118.26 52.614,118.26 C 64.834,118.26 78.929,107.226 81.566,93.248 C 83.58,82.589 57.867,86.86 58.791,93.913 L 58.791,93.913 z "
-              fill-opacity="1"
+              fillOpacity="1"
               fill="currentcolor"
               id="path3783"
             />
