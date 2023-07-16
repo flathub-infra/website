6cd8a8833d45606750d7556cfa91fdfe1e893f06
“claudio-code” <suporte2424@outlook.com>
2023-07-16 03:26:49 -0300

    added tests in redirect component of login

---
+++ frontend/__tests__/components/login/ProviderLink.test.tsx
@@ -0,0 +1,170 @@
+import { render, fireEvent, waitFor } from "@testing-library/react"
+import ProviderLink from "../../../src/components/login/ProviderLink"
+import { LoginProvider } from "src/types/Login"
+import React, { useState } from "react"
+import { toast } from "react-toastify"
+
+type Props = {
+  provider: LoginProvider
+  inACard?: boolean
+}
+
+const githubProvider: LoginProvider = {
+  method: "github",
+  name: "GitHub",
+}
+const githubProps: Props = {
+  inACard: true,
+  provider: githubProvider,
+}
+const translationMock = jest.fn()
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: translationMock,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    }
+  },
+}))
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    }
+  },
+}))
+
+const useStateSpy = jest.spyOn(React, "useState")
+const setStateMock = jest.fn()
+// @ts-ignore
+useStateSpy.mockImplementation((init) => [init, setStateMock])
+const useLocalStorageSpy = jest.spyOn(Storage.prototype, "setItem")
+
+jest.mock("react-toastify", () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+  },
+}))
+
+let location: Location
+beforeEach(() => {
+  location = window.location
+  jest.spyOn(window, "location", "get").mockRestore()
+  const mockedLocation = {
+    ...location,
+  }
+  jest.spyOn(window, "location", "get").mockReturnValue(mockedLocation)
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+test("redirect login with success", async () => {
+  const expectedRedirectValue = "https://wiki.gnome.org"
+  const expectedReturnToValue = '""'
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ redirect: expectedRedirectValue }),
+    }),
+  ) as jest.Mock
+
+  const { getByRole } = render(
+    <ProviderLink
+      provider={githubProps.provider}
+      inACard={githubProps.inACard}
+    />,
+  )
+
+  await waitFor(() => {
+    fireEvent.click(getByRole("button"))
+  })
+
+  expect(translationMock.mock.calls[0]).toEqual([
+    "login-with-provider",
+    { provider: "GitHub" },
+  ])
+  expect(setStateMock).toHaveBeenCalledWith(true)
+  expect(useLocalStorageSpy).toHaveBeenCalledWith(
+    "returnTo",
+    expectedReturnToValue,
+  )
+  expect(window.location.href).toEqual(expectedRedirectValue)
+})
+
+test("redirect login with api error", async () => {
+  global.fetch = jest.fn(() => Promise.reject()) as jest.Mock
+  const { getByRole } = render(
+    <ProviderLink
+      provider={githubProps.provider}
+      inACard={githubProps.inACard}
+    />,
+  )
+
+  await waitFor(() => {
+    fireEvent.click(getByRole("button"))
+  })
+
+  expect(setStateMock.mock.calls).toEqual([[true], [false]])
+  expect(translationMock).toHaveBeenCalledWith("network-error-try-again")
+  expect(toast.error).toHaveBeenCalled()
+})
+
+test("redirect login with response error", async () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: false,
+      json: () => Promise.resolve({}),
+    }),
+  ) as jest.Mock
+
+  const { getByRole } = render(
+    <ProviderLink
+      provider={githubProps.provider}
+      inACard={githubProps.inACard}
+    />,
+  )
+
+  await waitFor(() => {
+    fireEvent.click(getByRole("button"))
+  })
+  expect(setStateMock.mock.calls).toEqual([[true], [false]])
+  expect(toast.error).toHaveBeenCalled()
+})
+
+test("redirect login test if clicked is true not call api", async () => {
+  global.fetch = jest.fn() as jest.Mock
+  // @ts-ignore
+  useStateSpy.mockImplementation(() => [true, setStateMock])
+  const { getByRole } = render(
+    <ProviderLink
+      provider={githubProps.provider}
+      inACard={githubProps.inACard}
+    />,
+  )
+
+  await waitFor(() => {
+    fireEvent.click(getByRole("button"))
+    fireEvent.click(getByRole("button"))
+    fireEvent.click(getByRole("button"))
+  })
+  expect(global.fetch).not.toHaveBeenCalled()
+  expect(setStateMock).not.toHaveBeenCalled()
+})
