b65a85ea2b1307e9f9d80108dbcdf41bd0ea641a
“claudio-code” <suporte2424@outlook.com>
2023-07-18 19:11:57 -0300

    added setup of header and search

---
+++ frontend/__tests__/components/layout/Header.test.tsx
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react"
+import Header from "../../../src/components/layout/Header"
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    }
+  },
+}))
+
+const translationMock = jest.fn()
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: translationMock,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+        dir: jest.fn(),
+      },
+    }
+  },
+}))
+
+test("Header search", async () => {
+  render(<Header />)
+})
