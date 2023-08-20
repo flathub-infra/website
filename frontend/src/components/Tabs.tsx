e60b36ee6ab9249de8b2d65147b5c5d00020e06e
Razze <razzeee@gmail.com>
2023-08-20 15:18:37 +0200

    Scope layout animation

--- frontend/src/components/Tabs.tsx
+++ frontend/src/components/Tabs.tsx
@@ -2,7 +2,7 @@ import { Tab } from "@headlessui/react"
 import { Fragment, FunctionComponent } from "react"
 import { clsx } from "clsx"
 import Badge from "./application/Badge"
-import { motion } from "framer-motion"
+import { LayoutGroup, motion } from "framer-motion"
 
 interface Props {
   tabs: {
@@ -17,36 +17,38 @@ const Tabs: FunctionComponent<Props> = ({ tabs }) => {
   return (
     <>
       <Tab.Group>
-        <Tab.List className="flex gap-3 rounded-t-xl bg-flathub-white px-3 shadow-md dark:bg-flathub-arsenic">
-          {tabs.map((tab, index) => (
-            <Tab key={index} as={Fragment}>
-              {({ selected }) => (
-                <button
-                  className={clsx(
-                    "relative",
-                    "flex whitespace-nowrap px-1 py-4 text-sm font-medium transition focus:outline-none",
-                  )}
-                >
-                  {tab.name}
-                  {!!tab.badge && (
-                    <span className="ms-1">
-                      <Badge text={tab.badge.toString()}></Badge>
-                    </span>
-                  )}
-                  {selected && (
-                    <motion.div
-                      layoutId="tab-indicator"
-                      className={clsx(
-                        "absolute bottom-[-1px] left-0 right-0 h-1",
-                        "bg-flathub-celestial-blue",
-                      )}
-                    />
-                  )}
-                </button>
-              )}
-            </Tab>
-          ))}
-        </Tab.List>
+        <LayoutGroup id={tabs.map((tab) => tab.name).join("-")}>
+          <Tab.List className="flex gap-3 rounded-t-xl bg-flathub-white px-3 shadow-md dark:bg-flathub-arsenic">
+            {tabs.map((tab, index) => (
+              <Tab key={index} as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={clsx(
+                      "relative",
+                      "flex whitespace-nowrap px-1 py-4 text-sm font-medium transition focus:outline-none",
+                    )}
+                  >
+                    {tab.name}
+                    {!!tab.badge && (
+                      <span className="ms-1">
+                        <Badge text={tab.badge.toString()}></Badge>
+                      </span>
+                    )}
+                    {selected && (
+                      <motion.div
+                        layoutId="tab-indicator"
+                        className={clsx(
+                          "absolute bottom-[-1px] left-0 right-0 h-1",
+                          "bg-flathub-celestial-blue",
+                        )}
+                      />
+                    )}
+                  </button>
+                )}
+              </Tab>
+            ))}
+          </Tab.List>
+        </LayoutGroup>
         <Tab.Panels>
           {tabs.map((tab, index) => (
             <Tab.Panel
