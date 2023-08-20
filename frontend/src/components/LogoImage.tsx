45e3ad4bca4009dfccb6a507efda4eb61d1533b6
Razze <razzeee@gmail.com>
2023-08-21 01:26:03 +0200

    Make fallback logo slightly smaller

--- frontend/src/components/LogoImage.tsx
+++ frontend/src/components/LogoImage.tsx
@@ -22,7 +22,7 @@ const LogoImage: FunctionComponent<Props> = ({ iconUrl, appName }) => {
           aria-hidden
         />
       ) : (
-        <div className="flex h-full w-full items-center justify-center dark:invert">
+        <div className="flex h-full w-full items-center justify-center dark:invert p-1">
           <Image
             src="/img/logo/flathub-logo-mini.svg"
             alt={t("app-logo", { "app-name": appName })}
