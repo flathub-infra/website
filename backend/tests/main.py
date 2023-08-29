03f1a928bf76f830e553337e187b91d0a4bfa5d6
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-08-29 20:02:07 +0200

    fixup! Update tests

--- backend/tests/main.py
+++ backend/tests/main.py
@@ -113,12 +113,6 @@ def test_apps_by_category_with_too_few_per_page_params(client):
     assert response.status_code == 400
 
 
-def test_apps_by_developer(client):
-    response = client.get("/developer/Sugar Labs Community")
-    assert response.status_code == 200
-    assert response.json() == _get_expected_json_result("test_apps_by_developer")
-
-
 def test_apps_by_non_existent_developer(client):
     response = client.get("/developer/NonExistent")
     assert response.status_code == 200
