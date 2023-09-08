b7e444f977a368f27938b96f0587c6e456524bc0
Razze <razzeee@gmail.com>
2023-09-08 20:14:03 +0200

    Fix display of permission tile
    Closes #1912

--- frontend/src/components/application/HorizontalStackedListBox.tsx
+++ frontend/src/components/application/HorizontalStackedListBox.tsx
@@ -4,7 +4,7 @@ const Item = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
   return <li className={`flex justify-center w-full`}>{children}</li>
 }
 
-export const VerticalStackedListBox = ({
+export const HorizontalStackedListBox = ({
   children,
 }: {
   children: JSX.Element[] | JSX.Element
@@ -14,7 +14,7 @@ export const VerticalStackedListBox = ({
       className={clsx(
         "flex flex-col sm:flex-row rounded-xl flex-grow",
         "shadow-md dark:bg-flathub-arsenic dark:divide-flathub-dark-gunmetal",
-        "sm:divide-x-4 sm:divide-y-0 divide-y-4",
+        "sm:divide-x-2 sm:divide-y-0 divide-y-2",
       )}
     >
       {Array.isArray(children) ? (
