cc3bc29ec88ccd9d805b050ac52a6d1141a77b84
Razze <razzeee@gmail.com>
2023-09-16 20:13:18 +0200

    Fix format

--- frontend/src/safety.ts
+++ frontend/src/safety.ts
@@ -298,7 +298,7 @@ export function getSafetyRating(
 function addFileSafetyRatings(summary: Summary): AppSafetyRating[] {
   // Implements https://gitlab.gnome.org/GNOME/gnome-software/-/blob/9ae6d604297cd946ab45c11f7d6c25461cb119c9/plugins/flatpak/gs-flatpak.c#L319
   const appSafetyRating: AppSafetyRating[] = []
-  
+
   // read/write all your data
   if (
     summary.metadata.permissions.filesystems?.some(
