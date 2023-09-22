9643d3f24255cdef8c900b05e03c1f9b881806b6
Kolja Lampe <razzeee@gmail.com>
2023-09-22 09:22:00 +0200

    Add needed to pass since for future changes

--- backend/app/quality_moderation.py
+++ backend/app/quality_moderation.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass
 
 from fastapi import APIRouter, Depends, FastAPI, HTTPException
@@ -15,6 +16,7 @@ router = APIRouter(prefix="/quality-moderation", default_response_class=ORJSONRe
 class Guideline:
     id: str
     url: str
+    needed_to_pass_since: datetime.datetime
     read_only: bool = False
 
 
@@ -31,6 +33,7 @@ GUIDELINES = [
             Guideline(
                 "general-no-trademark-violations",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#no-trademark-violations",
+                datetime.datetime(2023, 9, 1),
             ),
         ],
     ),
@@ -40,26 +43,32 @@ GUIDELINES = [
             Guideline(
                 "app-icon-size",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#icon-size",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-icon-footprint",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#reasonable-footprint",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-icon-contrast",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#good-contrast",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-icon-detail",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#not-too-much-detail",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-icon-no-baked-in-shadows",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#no-baked-in-shadows",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-icon-in-line-with-contemporary-styles",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#in-line-with-contemporary-styles",
+                datetime.datetime(2023, 9, 1),
             ),
         ],
     ),
@@ -69,15 +78,18 @@ GUIDELINES = [
             Guideline(
                 "app-name-not-too-long",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#not-too-long",
+                datetime.datetime(2023, 9, 1),
                 read_only=True,
             ),
             Guideline(
                 "app-name-just-the-name",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#just-the-name",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-no-weird-formatting",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#no-weird-formatting",
+                datetime.datetime(2023, 9, 1),
             ),
         ],
     ),
@@ -87,23 +99,28 @@ GUIDELINES = [
             Guideline(
                 "app-summary-not-too-long",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#not-too-long-1",
+                datetime.datetime(2023, 9, 1),
                 read_only=True,
             ),
             Guideline(
                 "app-summary-not-technical",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#not-technical",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-summary-no-weird-formatting",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#no-weird-formatting-1",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-summary-dont-repeat-app-name",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#dont-repeat-the-name",
+                datetime.datetime(2023, 9, 1),
             ),
             Guideline(
                 "app-summary-dont-start-with-an-article",
                 "https://docs.flathub.org/docs/for-app-authors/appdata-guidelines/quality-guidelines/#dont-start-with-an-article",
+                datetime.datetime(2023, 9, 1),
             ),
         ],
     ),
