d6b7e411b323e8b90a67d65cd167876e4cc979fb
Razze <razzeee@gmail.com>
2023-09-28 00:01:37 +0200

    Make quality header useful

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -234,4 +234,6 @@ export const MODERATION_REVIEW_URL = (id: number) =>
 export const QUALITY_MODERATION_BASE_URL: string = `${BASE_URI}/quality-moderation`
 export const QUALITY_MODERATION_APP = (appid: string) =>
   `${QUALITY_MODERATION_BASE_URL}/${appid}`
+export const QUALITY_MODERATION_APP_STATUS = (appid: string) =>
+  `${QUALITY_MODERATION_BASE_URL}/${appid}/status`
 export const QUALITY_MODERATION_DASHBOARD: string = `${QUALITY_MODERATION_BASE_URL}/status`
