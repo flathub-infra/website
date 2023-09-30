0abddbf66f65f6d41052675d157c3aac1588f7d0
Razze <razzeee@gmail.com>
2023-09-30 16:33:49 +0200

    Remove stray log

--- frontend/src/components/application/QualityModerationSlideOver.tsx
+++ frontend/src/components/application/QualityModerationSlideOver.tsx
@@ -59,8 +59,6 @@ const QualityCategories = ({
     },
   })
 
-  console.log(query.data.data)
-
   return (
     <div className="flex flex-col gap-4 dark:divide-flathub-granite-gray">
       <Button
