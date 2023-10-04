0880d44ab715abac33fac54957ea57c3903e7fb7
Razze <razzeee@gmail.com>
2023-10-04 22:58:17 +0200

    Set permissions policy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -119,6 +119,11 @@ const nextConfig = (phase) => ({
             value: "strict-origin-when-cross-origin",
           },
           {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(self), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()",
+          },
+          {
             key: "Content-Security-Policy",
             value:
               /**
