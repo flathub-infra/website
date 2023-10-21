ee746a424c4dc35e03221cf2729c685fe8326e96
Razze <razzeee@gmail.com>
2023-10-21 02:43:51 +0200

    Allow theme switching in storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-themes",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-react-i18next",
