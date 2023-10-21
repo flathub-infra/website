5beef3de31721c0df906b4404380e973ea13aa5e
Razze <razzeee@gmail.com>
2023-10-21 03:34:21 +0200

    Style badges correct inside cards

--- frontend/src/components/application/Badge.tsx
+++ frontend/src/components/application/Badge.tsx
@@ -1,12 +1,21 @@
+import clsx from "clsx"
 import { FunctionComponent } from "react"
 
 interface Props {
   text: string
+  inACard?: boolean
 }
 
-const Tags: FunctionComponent<Props> = ({ text }) => {
+const Tags: FunctionComponent<Props> = ({ text, inACard }) => {
   return (
-    <span className="inline-flex items-center rounded-full bg-flathub-gainsborow px-1.5 py-0.5 text-xs font-medium text-flathub-sonic-silver dark:bg-flathub-arsenic dark:text-flathub-spanish-gray">
+    <span
+      className={clsx(
+        "inline-flex items-center rounded-full px-1.5 py-0.5 text-xs font-medium",
+        inACard
+          ? "text-flathub-sonic-silver dark:text-flathub-spanish-gray bg-flathub-gainsborow dark:bg-flathub-dark-gunmetal"
+          : "text-flathub-sonic-silver dark:bg-flathub-arsenic dark:text-flathub-spanish-gray bg-flathub-gainsborow ",
+      )}
+    >
       {text}
     </span>
   )
