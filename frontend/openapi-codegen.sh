edfe280f6a525f9f231dbe7e5fab5136c7434a54
Kyle Mckay <kymckay.dev@gmail.com>
2023-10-22 09:19:27 +0100

    feat: add api client codegen capability
    - Using OpenAPI generator to generate an API client from the OpenAPI
      spec exposed by the backend. Should ensure correct typing and
      encourage us to use better implementation habits.
    - Shell script to run codegen and cleanup. Added to package.json.
    - VSCode task to conveniently run the codegen.

---
+++ frontend/openapi-codegen.sh
@@ -0,0 +1,15 @@
+# Clean slate
+rm -rf ./src/codegen
+
+npx @openapitools/openapi-generator-cli generate \
+  -i http://localhost:8000/openapi.json -o src/codegen \
+  -g typescript-axios --additional-properties=supportsES6=true
+
+# Tidy up unnecessary artifacts
+# Not interested in publishing as a separate npm package
+rm -rf ./openapitools.json \
+  ./src/codegen/.openapi-generator \
+  ./src/codegen/.openapi-generator-ignore \
+  ./src/codegen/.npmignore \
+  ./src/codegen/.gitignore \
+  ./src/codegen/git_push.sh
