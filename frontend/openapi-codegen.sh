89598d8ae1b21d88affceaf780cd3271c3122744
Razze <razzeee@gmail.com>
2023-10-22 14:16:41 +0200

    Run prettier on generated files

--- frontend/openapi-codegen.sh
+++ frontend/openapi-codegen.sh
@@ -1,8 +1,11 @@
 # Clean slate
 rm -rf ./src/codegen
 
+export TS_POST_PROCESS_FILE="prettier --write --ignore-path .gitignore"
+
 npx @openapitools/openapi-generator-cli generate \
   -i http://localhost:8000/openapi.json -o src/codegen \
+  --enable-post-process-file \
   -g typescript-axios --additional-properties=supportsES6=true
 
 # Tidy up unnecessary artifacts
