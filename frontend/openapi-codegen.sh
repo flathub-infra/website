fde4989f75c2134df20aaa55e0e1163a3edc4de1
Kyle Mckay <kymckay.dev@gmail.com>
2023-10-22 13:39:07 +0100

    Use ignore file to skip unwanted file generation
    This is a slightly nicer development experience since we aren't wiping
    the generated directory every time (causing the IDE to temporarily
    detect bad imports).
    
    Still need to remove generator artifacts that aren't skippable with this file.

--- frontend/openapi-codegen.sh
+++ frontend/openapi-codegen.sh
@@ -1,6 +1,4 @@
 # Clean slate
-rm -rf ./src/codegen
-
 export TS_POST_PROCESS_FILE="prettier --write --ignore-path .gitignore"
 
 npx @openapitools/openapi-generator-cli generate \
@@ -8,11 +6,5 @@ npx @openapitools/openapi-generator-cli generate \
   --enable-post-process-file \
   -g typescript-axios --additional-properties=supportsES6=true
 
-# Tidy up unnecessary artifacts
-# Not interested in publishing as a separate npm package
-rm -rf ./openapitools.json \
-  ./src/codegen/.openapi-generator \
-  ./src/codegen/.openapi-generator-ignore \
-  ./src/codegen/.npmignore \
-  ./src/codegen/.gitignore \
-  ./src/codegen/git_push.sh
+# Tidy up generator artifacts
+rm -rf openapitools.json src/codegen/.openapi-generator
