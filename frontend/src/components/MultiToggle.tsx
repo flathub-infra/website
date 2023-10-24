b5ad4d2d3f9a2ed6e9605f09d01f49452e135211
Razze <razzeee@gmail.com>
2023-10-24 12:18:01 +0200

    Add some filters for the quality moderation dashboard

--- frontend/src/components/MultiToggle.tsx
+++ frontend/src/components/MultiToggle.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react"
 
 const MultiToggle = ({
   items,
+  size,
 }: {
   items: {
     id: string
@@ -13,10 +14,18 @@ const MultiToggle = ({
     disabled?: boolean
     color?: string
   }[]
+  size: "sm" | "lg"
 }) => {
   return (
     <LayoutGroup id={Math.random().toString(36)}>
-      <ul className="flex w-full cursor-pointer justify-around rounded-full border border-flathub-gray-x11 dark:border-flathub-lotion/10 h-[34px] py-1 text-center">
+      <ul
+        className={clsx(
+          "flex w-full cursor-pointer justify-around rounded-full",
+          "border border-flathub-gray-x11 dark:border-flathub-lotion/10",
+          "py-1 text-center",
+          size === "sm" ? "h-[34px]" : "h-14",
+        )}
+      >
         {items.map((item) => (
           <div key={item.id} className="relative z-10 mx-1 w-full">
             <button
@@ -29,8 +38,7 @@ const MultiToggle = ({
                   : "enabled:text-flathub-gainsborow enabled:hover:bg-flathub-spanish-gray enabled:hover:text-flathub-gainsborow enabled:dark:text-flathub-lotion enabled:dark:hover:bg-flathub-granite-gray enabled:dark:hover:text-flathub-lotion",
                 item.disabled &&
                   "cursor-not-allowed text-flathub-gainsborow dark:text-flathub-arsenic",
-                "h-fit w-fit rounded-full transition",
-                "enabled:active:bg-flathub-pastel-pastel-grapefruit dark:enabled:active:bg-flathub-black",
+                "h-full w-full rounded-full transition",
               )}
             >
               <li>{item.content}</li>
