55709381df17f92c166b108cd8f9ccbdc9c532f1
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-10-25 00:53:09 +0300

    fix(size): replaced SI prefixes with IEC prefixes

--- frontend/src/size.ts
+++ frontend/src/size.ts
@@ -1,9 +1,31 @@
+function roundNumber(number: number, digits: number) {
+  const multiple = Math.pow(10, digits)
+  return Math.floor(number * multiple + 0.50000000000001) / multiple
+}
+
 export function calculateHumanReadableSize(size: number) {
-  if (size > 1_000_000_000) {
-    return (size / 1_000_000_000).toFixed(2) + " GB"
-  } else if (size > 1_000_000) {
-    return Math.round(size / 1_000_000) + " MB"
-  } else {
-    return Math.round(size / 1_000) + " KB"
+  // (Maximum) number of digits to round to after floating point.
+  const ROUND_DIGITS = 2
+
+  // First letters of multiples of 2, more precisely of 2^10.
+  const UNIT_LETTERS = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+
+  const i = "i" // From "binary".
+  const POW10 = 1024 // 2 to the power of 10.
+
+  if (size < POW10) {
+    // Explicit whitespace for good measures.
+    return `${size}` + " " + `${UNIT_LETTERS[0]}`
   }
+  let pow10Count = 0 // Shows which multiple of 2^10 the `value` is (i.e., 2^10^0).
+  let value = size
+  while (value >= POW10 && pow10Count < UNIT_LETTERS.length - 1) {
+    value /= POW10
+    pow10Count++
+  }
+  return (
+    `${roundNumber(value, ROUND_DIGITS)}` +
+    " " +
+    `${UNIT_LETTERS[pow10Count]}${i}${UNIT_LETTERS[0]}`
+  )
 }
