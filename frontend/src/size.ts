4de8a2c235b530d98223b837c3b9f90b4c46b075
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-10-27 14:11:45 +0300

    perf(size): used log_1024(size) instead of loop

--- frontend/src/size.ts
+++ frontend/src/size.ts
@@ -6,13 +6,12 @@ export function calculateHumanReadableSize(size: number) {
   if (size < POW10) {
     return size.toString() + " " + UNIT_CHARS[0].toString()
   }
-  // Shows which multiple of 2^10 the `value` is (i.e., 2^10^0).
-  let pow10Count = 0
-  let value = size
-  while (value >= POW10 && pow10Count < UNIT_CHARS.length - 1) {
-    value /= POW10
-    pow10Count++
-  }
+  // Shows which multiple of 2^10 the `size` is (i.e., 2^10^0).
+  const pow10Count = Math.min(
+    Math.floor(Math.log(size) / Math.log(POW10)),
+    UNIT_CHARS.length - 1,
+  )
+  const value = size / Math.pow(POW10, pow10Count)
   return (
     // .toFixed() will round to fixed number of digits,
     // parseFloat() will convert strings like 1.00 to 1 and 1.10 to 1.1.
