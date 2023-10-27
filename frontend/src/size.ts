a7ee7d009f282ff22bf5476d06c040f6a473cdfc
Andrew Voynov <37143421+Andrew15-5@users.noreply.github.com>
2023-10-27 15:22:11 +0300

    fix(size): replaced SI prefixes with IEC prefixes (#2141)

--- frontend/src/size.ts
+++ frontend/src/size.ts
@@ -1,9 +1,22 @@
 export function calculateHumanReadableSize(size: number) {
-  if (size > 1_000_000_000) {
-    return (size / 1_000_000_000).toFixed(2) + " GB"
-  } else if (size > 1_000_000) {
-    return Math.round(size / 1_000_000) + " MB"
-  } else {
-    return Math.round(size / 1_000) + " KB"
+  // First letters of multiples of 2, more precisely of 2^10.
+  const UNIT_CHARS = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+  const i = "i" // From "binary".
+  const POW10 = 1024 // 2^10
+  if (size < POW10) {
+    return size.toString() + " " + UNIT_CHARS[0].toString()
   }
+  // Shows which multiple of 2^10 the `size` is (i.e., 2^10^0).
+  const pow10Count = Math.min(
+    Math.floor(Math.log(size) / Math.log(POW10)),
+    UNIT_CHARS.length - 1,
+  )
+  const value = size / Math.pow(POW10, pow10Count)
+  return (
+    // .toFixed() will round to fixed number of digits,
+    // parseFloat() will convert strings like 1.00 to 1 and 1.10 to 1.1.
+    parseFloat(value.toFixed(2)).toString() +
+    " " +
+    `${UNIT_CHARS[pow10Count]}${i}${UNIT_CHARS[0]}`
+  )
 }
