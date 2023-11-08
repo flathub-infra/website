9679fbb54bc1a79fdde4b49cfc9472598e7000ad
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-11-08 12:35:39 +0100

    Re-format with black

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -62,9 +62,7 @@ def appstream2dict(appstream_base_url=None) -> dict[str, dict]:
         r = requests.get(appstream_url, stream=True)
         appstream = gzip.decompress(r.raw.data)
     else:
-        appstream_url = (
-            "https://hub.flathub.org/repo/appstream/x86_64/appstream.xml.gz"
-        )
+        appstream_url = "https://hub.flathub.org/repo/appstream/x86_64/appstream.xml.gz"
         r = requests.get(appstream_url, stream=True)
         appstream = gzip.decompress(r.raw.data)
 
