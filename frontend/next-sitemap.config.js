7d95f88e2572e2114219b5d6fbe5dcbb80d5effc
JakobDev <jakobdev@gmx.de>
2023-12-09 10:27:20 +0100

    Block more AI Bots

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -13,6 +13,26 @@ module.exports = {
         userAgent: "CCBot",
         disallow: ["/"],
       },
+      {
+        userAgent: "Google-Extended",
+        disallow: ["/"],
+      },
+      {
+        userAgent: "anthropic-ai",
+        disallow: ["/"],
+      },
+      {
+        userAgent: "Omgilibot",
+        disallow: ["/"],
+      },
+      {
+        userAgent: "Omgili",
+        disallow: ["/"],
+      },
+      {
+        userAgent: "FacebookBot",
+        disallow: ["/"],
+      },
     ],
     additionalSitemaps: [
       `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/server-sitemap-index.xml`,
