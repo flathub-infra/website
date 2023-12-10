d4b207d0a52308dc00b6df4c648d7c7e76712344
Kolja Lampe <razzeee@gmail.com>
2023-12-11 00:04:44 +0100

    Update sentry config

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -1,5 +1,5 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs"
@@ -10,11 +10,10 @@ Sentry.init({
   dsn:
     SENTRY_DSN ||
     "https://b512f563700847e787e978ae1c15133c@o467221.ingest.sentry.io/6610580",
+
   // Adjust this value in production, or use tracesSampler for greater control
-  sampleRate: 0.1,
   tracesSampleRate: 0.1,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 })
