dd06ef74ae1e455dc1764f0db57d4c5642c742ce
Kolja Lampe <razzeee@gmail.com>
2023-12-24 03:33:14 +0100

    Fix wrong translation imports

--- frontend/src/components/application/AppUploadControls/UploadTokenControls.tsx
+++ frontend/src/components/application/AppUploadControls/UploadTokenControls.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, useCallback, useState } from "react"
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "next-i18next"
 import Button from "src/components/Button"
 import NewTokenDialog from "./NewTokenDialog"
 import {
