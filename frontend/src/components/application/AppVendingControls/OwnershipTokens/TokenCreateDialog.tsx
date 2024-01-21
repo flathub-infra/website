8cad09e33456d3ca812b3a9b0a01a2f36c83ef43
Kolja Lampe <razzeee@gmail.com>
2024-01-21 04:50:31 +0100

    Use modal component in more places

--- frontend/src/components/application/AppVendingControls/OwnershipTokens/TokenCreateDialog.tsx
+++ frontend/src/components/application/AppVendingControls/OwnershipTokens/TokenCreateDialog.tsx
@@ -1,9 +1,9 @@
-import { Dialog, Transition } from "@headlessui/react"
 import { useTranslation } from "next-i18next"
-import { Fragment, FunctionComponent, useCallback, useState } from "react"
+import { FunctionComponent, useCallback, useState } from "react"
 import { Appstream } from "../../../../types/Appstream"
 import Button from "../../../Button"
 import { vendingApi } from "src/api"
+import Modal from "src/components/Modal"
 
 interface Props {
   app: Appstream
@@ -41,30 +41,22 @@ const TokenCreateDialog: FunctionComponent<Props> = ({
   return (
     <>
       <Button onClick={() => setShown(true)}>{t("create-tokens")}</Button>
-      <Transition appear show={shown} as={Fragment}>
-        <Dialog
-          as="div"
-          onClose={() => {
-            setShown(false)
-            setText("")
-          }}
-        >
-          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-
-          <div className="fixed inset-0 flex items-center justify-center p-4">
-            <Dialog.Panel className="inline-flex flex-col justify-center space-y-6 rounded-xl bg-flathub-gainsborow p-14 shadow-md dark:bg-flathub-dark-gunmetal">
-              <Dialog.Title className="m-0">{t("create-tokens")}</Dialog.Title>
-              <textarea
-                placeholder={t("token-creation-placeholder")}
-                value={text}
-                onChange={textUpdate}
-                className="h-40 rounded-xl p-3"
-              />
-              <Button onClick={onSubmit}>{t("submit")}</Button>
-            </Dialog.Panel>
-          </div>
-        </Dialog>
-      </Transition>
+      <Modal
+        shown={shown}
+        title={t("create-tokens")}
+        onClose={() => {
+          setShown(false)
+          setText("")
+        }}
+        submitButton={{ onClick: onSubmit }}
+      >
+        <textarea
+          placeholder={t("token-creation-placeholder")}
+          value={text}
+          onChange={textUpdate}
+          className="h-40 rounded-xl p-3"
+        />
+      </Modal>
     </>
   )
 }
