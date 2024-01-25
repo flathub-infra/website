b5167e5fab3a66bb858ecdc2073eef1bd6c5d651
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-01-25 13:37:48 +0100

    upload_tokens: Explicitly refresh dev_flatpaks before ownership check

--- backend/app/upload_tokens.py
+++ backend/app/upload_tokens.py
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 
 from . import config, models, utils, worker
 from .emails import EmailCategory, EmailInfo
-from .logins import login_state
+from .logins import login_state, do_refresh_dev_flatpaks
 
 router = APIRouter(prefix="/upload-tokens")
 
@@ -138,16 +138,17 @@ def create_upload_token(
 
     if not login.state.logged_in():
         raise HTTPException(status_code=401, detail=ErrorDetail.NOT_LOGGED_IN)
-    if app_id not in login.user.dev_flatpaks(sqldb):
+
+    dev_flatpaks = do_refresh_dev_flatpaks()
+    if app_id not in dev_flatpaks.get("dev_flatpaks", []):
         raise HTTPException(status_code=403, detail=ErrorDetail.NOT_APP_DEVELOPER)
 
     if direct_upload_app := models.DirectUploadApp.by_app_id(sqldb, app_id):
         if direct_upload_app.archived:
             raise HTTPException(status_code=403, detail=ErrorDetail.APP_ARCHIVED)
-
-    if "stable" in request.repos:
+    else:
         # Only direct upload apps may create tokens for the stable repo.
-        if direct_upload_app is None:
+        if "stable" in request.repos:
             raise HTTPException(
                 status_code=403,
                 detail=ErrorDetail.NOT_DIRECT_UPLOAD_APP,
