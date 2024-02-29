5fb56b497b1c5ccaeb55f8645cad31d7821983c7
Kolja Lampe <razzeee@gmail.com>
2024-02-29 17:20:47 +0100

    Codegen new types for frontend

---
+++ backend/alembic/versions/5c6c6459f661_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5c6c6459f661
+Revises: 91123486fcc5, d74f7d372dd2
+Create Date: 2024-04-15 08:54:56.069733
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c6c6459f661'
+down_revision = ('91123486fcc5', 'd74f7d372dd2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
