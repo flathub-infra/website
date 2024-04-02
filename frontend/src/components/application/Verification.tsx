ab3d02310641466e11f4622a37d2105b588c42f2
Razze <razzeee@gmail.com>
2024-04-02 23:14:41 +0200

    Use correct import

--- frontend/src/components/application/Verification.tsx
+++ frontend/src/components/application/Verification.tsx
@@ -5,7 +5,7 @@ import { VerificationStatus } from "src/types/VerificationStatus"
 import { VerificationText } from "src/verification"
 import VerificationIcon from "./VerificationIcon"
 import { HiMiniExclamationTriangle } from "react-icons/hi2"
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "next-i18next"
 
 interface Props {
   appId: string
