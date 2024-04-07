479522d6bde6d94fd4a89773bdab204f7497a460
Razze <razzeee@gmail.com>
2024-04-07 22:55:52 +0200

    New start page sections (only on index-new)

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -15,7 +15,7 @@ const Tile: FunctionComponent<Props> = forwardRef<HTMLAnchorElement, Props>(
         className={clsx(
           "flex items-center justify-center break-words rounded-xl p-4 text-center duration-500",
           "bg-flathub-white text-flathub-sonic-silver shadow-md dark:bg-flathub-arsenic/70 dark:text-flathub-spanish-gray",
-          "hover:bg-flathub-gainsborow/20 hover:no-underline hover:shadow-xl dark:hover:bg-flathub-arsenic/90",
+          "hover:bg-flathub-gainsborow/20 hover:no-underline hover:shadow-xl dark:hover:bg-flathub-arsenic/90 hover:cursor-pointer",
           "active:bg-flathub-gainsborow/40 active:shadow-sm active:dark:bg-flathub-arsenic",
           className,
         )}
