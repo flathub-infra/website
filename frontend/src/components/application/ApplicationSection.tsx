cdee380524b5e6a5c7378a8b426d0024b5ee3413
Razze <razzeee@gmail.com>
2024-04-14 13:37:13 +0200

    Move more button to bottom of section on new index page

--- frontend/src/components/application/ApplicationSection.tsx
+++ frontend/src/components/application/ApplicationSection.tsx
@@ -6,6 +6,7 @@ import ApplicationCard from "./ApplicationCard"
 import { useTranslation } from "next-i18next"
 import ButtonLink from "../ButtonLink"
 import { motion } from "framer-motion"
+import clsx from "clsx"
 
 interface Props {
   href: string
@@ -13,6 +14,7 @@ interface Props {
   applications: AppstreamListItem[]
   appSelection?: ReactElement
   showMore?: boolean
+  morePosition?: "top" | "bottom"
 }
 
 const ApplicationSection: FunctionComponent<Props> = ({
@@ -21,6 +23,7 @@ const ApplicationSection: FunctionComponent<Props> = ({
   applications,
   appSelection,
   showMore = true,
+  morePosition = "top",
 }) => {
   const { t } = useTranslation()
 
@@ -31,7 +34,7 @@ const ApplicationSection: FunctionComponent<Props> = ({
       <header className="mb-3 flex max-w-full flex-row content-center justify-between">
         <h2 className="my-auto text-2xl font-bold">{title}</h2>
 
-        {showMore && (
+        {showMore && morePosition === "top" && (
           <ButtonLink
             href={href}
             passHref
@@ -57,6 +60,26 @@ const ApplicationSection: FunctionComponent<Props> = ({
           </motion.div>
         ))}
       </div>
+      {showMore && morePosition === "bottom" && (
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
+          key={title}
+          className={clsx("flex flex-row justify-center", "mt-3")}
+        >
+          <ButtonLink
+            href={href}
+            passHref
+            variant="secondary"
+            className="rounded-full"
+            aria-label={t("more-type", { type: title })}
+            title={t("more-type", { type: title })}
+          >
+            {t("more-type", { type: title })}
+          </ButtonLink>
+        </motion.div>
+      )}
     </div>
   )
 }
