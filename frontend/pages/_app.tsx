9de3a87d8353f9ba348b3668284b5173af7de3ec
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-04-23 23:21:54 +0200

    Hardcode baseURL

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -44,7 +44,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   axios.interceptors.request.use((config) => {
     return {
       ...config,
-      baseURL: process.env.NEXT_PUBLIC_API_BASE_URI,
+      baseURL: "https://flathub.org",
     }
   })
 
