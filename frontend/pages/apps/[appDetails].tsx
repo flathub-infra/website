fa43beed51aa41de7b6464601e4ab693e17c9ee3
Razze <razzeee@gmail.com>
2024-05-25 20:37:22 +0200

    Select language for og image

--- frontend/pages/apps/[appDetails].tsx
+++ frontend/pages/apps/[appDetails].tsx
@@ -39,6 +39,7 @@ export default function Details({
   verificationStatus,
   eolMessage,
   addons,
+  locale,
 }: {
   app: DesktopAppstream
   summary?: Summary
@@ -47,6 +48,7 @@ export default function Details({
   verificationStatus: VerificationStatus
   eolMessage: string
   addons: AddonAppstream[]
+  locale: string
 }) {
   const { t } = useTranslation()
   const [isQualityModalOpen, setIsQualityModalOpen] = useState(false)
@@ -64,7 +66,7 @@ export default function Details({
           url: `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/apps/${app?.id}`,
           images: [
             {
-              url: `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/api/appOgImage/${app?.id}`,
+              url: `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/api/appOgImage/${app?.id}?locale=${locale}`,
               height: 628,
               width: 1200,
               alt: app?.name,
@@ -169,6 +171,7 @@ export const getStaticProps: GetStaticProps = async ({
       verificationStatus,
       eolMessage,
       addons,
+      locale,
     },
     revalidate: 900,
   }
