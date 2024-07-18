9eee00a4866e8910b827fa1fa6eacea334188298
Kolja Lampe <razzeee@gmail.com>
2024-07-18 02:05:03 +0200

    Use image component for carousel

---
+++ frontend/src/components/application/CarouselNextJsImage.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import {
+  isImageFitCover,
+  isImageSlide,
+  useLightboxProps,
+  useLightboxState,
+} from "yet-another-react-lightbox"
+
+function isNextJsImage(slide) {
+  return (
+    isImageSlide(slide) &&
+    typeof slide.width === "number" &&
+    typeof slide.height === "number"
+  )
+}
+
+export default function CarouselNextJsImage({ slide, offset, rect }) {
+  const {
+    on: { click },
+    carousel: { imageFit },
+  } = useLightboxProps()
+
+  const { currentIndex } = useLightboxState()
+
+  const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit)
+
+  if (!isNextJsImage(slide)) return undefined
+
+  const width = !cover
+    ? Math.round(
+        Math.min(rect.width, (rect.height / slide.height) * slide.width),
+      )
+    : rect.width
+
+  const height = !cover
+    ? Math.round(
+        Math.min(rect.height, (rect.width / slide.width) * slide.height),
+      )
+    : rect.height
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        fill
+        alt=""
+        src={slide}
+        loading="eager"
+        draggable={false}
+        placeholder={slide.blurDataURL ? "blur" : undefined}
+        style={{
+          objectFit: cover ? "cover" : "contain",
+          cursor: click ? "pointer" : undefined,
+        }}
+        sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
+        onClick={
+          offset === 0 ? () => click?.({ index: currentIndex }) : undefined
+        }
+      />
+    </div>
+  )
+}
