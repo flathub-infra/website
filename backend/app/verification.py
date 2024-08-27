b094cf4a30cca930cd8f11ca99723a3950508b7a
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-08-27 12:04:20 +0200

    Add mapping for org.gnome.PowerStats

--- backend/app/verification.py
+++ backend/app/verification.py
@@ -173,6 +173,8 @@ def _get_gnome_doap_maintainers(app_id: str, group: str = "world") -> list[str]:
             repo_name = "gnome-characters"
         case "org.gnome.Nibbles":
             repo_name = "gnome-nibbles"
+        case "org.gnome.PowerStats":
+            repo_name = "gnome-power-manager"
         case _:
             repo_name = None
 
