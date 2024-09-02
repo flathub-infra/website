deab36bb729023174f665295733a1a741c276b0f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-09-02 15:34:48 +0200

    Add github.com to CSP

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ const CONTENT_SECURITY_POLICY = `
   script-src 'self' 'sha256-eMuh8xiwcX72rRYNAGENurQBAcH7kLlAUQcoOri3BIo=' https://webstats.gnome.org https://js.stripe.com;
   style-src 'self' 'unsafe-inline' https://dl.flathub.org;
   font-src 'self';
-  connect-src 'self' https://flathub.org https://webstats.gnome.org https://api.stripe.com https://o467221.ingest.sentry.io/api/6610580/;
+  connect-src 'self' https://flathub.org https://webstats.gnome.org https://api.stripe.com https://o467221.ingest.sentry.io/api/6610580/ https://github.com;
   img-src 'self' https://dl.flathub.org https://webstats.gnome.org https://avatars.githubusercontent.com https://gitlab.com https://gitlab.gnome.org https://lh3.googleusercontent.com https://secure.gravatar.com https://invent.kde.org data:;
   frame-ancestors 'none';
   frame-src 'self' https://js.stripe.com https://hooks.stripe.com;
