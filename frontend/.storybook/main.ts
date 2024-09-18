66ff8350bad6257a061355c7b0881d6bc511f3c1
Razze <razzeee@gmail.com>
2024-09-19 01:14:54 +0200

    Add accessability addon for storybook

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-themes",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     "storybook-addon-mock-date",
   ],
   framework: {
