ad3c672906c738db72a8024d27fd8abb0e18501d
Kolja Lampe <razzeee@gmail.com>
2024-10-12 01:17:55 +0200

    Expect typescript error

--- backend-node/emails/base.tsx
+++ backend-node/emails/base.tsx
@@ -54,6 +54,7 @@ export const Base = ({
       <Head />
       <Preview>{previewText}</Preview>
       <Tailwind
+        // @ts-expect-error this can be removed when https://github.com/resend/react-email/issues/1616 is fixed
         config={{
           theme: {
             extend: {
