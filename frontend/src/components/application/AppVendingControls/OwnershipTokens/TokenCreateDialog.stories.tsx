f1a8d2b771434647bf668e0cc561b33256612c08
Razze <razzeee@gmail.com>
2024-10-13 16:30:50 +0200

    Add stories where textareas are used

---
+++ frontend/src/components/application/AppVendingControls/OwnershipTokens/TokenCreateDialog.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import TokenCreateDialog from "./TokenCreateDialog"
+import { expect, userEvent, waitFor, within } from "@storybook/test"
+import React from "react"
+
+const meta = {
+  component: TokenCreateDialog,
+} satisfies Meta<typeof TokenCreateDialog>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <div style={{ height: "900px" }}>
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    app: {
+      id: "tv.abc.TestApp",
+    },
+    updateCallback: () => {},
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const button = canvas.getByRole("button")
+
+    await userEvent.click(button)
+
+    await waitFor(() => {
+      expect(canvas.getByText("Create Tokens")).toBeInTheDocument()
+    })
+  },
+}
