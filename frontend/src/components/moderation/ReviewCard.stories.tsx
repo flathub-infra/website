a23e67b35f0a435917fb239d3e503c0c74457944
Razze <razzeee@gmail.com>
2024-10-13 16:36:11 +0200

    Add reviewcard story

--- frontend/src/components/moderation/ReviewCard.stories.tsx
+++ frontend/src/components/moderation/ReviewCard.stories.tsx
@@ -3,8 +3,10 @@ import { Meta } from "@storybook/react"
 import ReviewCard from "./ReviewCard"
 import { t } from "i18next"
 import { faker } from "@faker-js/faker"
-import { ModerationRequestResponse } from "../../codegen/model"
+import { ModerationRequestResponse, Permission } from "../../codegen/model"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { UserContext } from "../../context/user-info"
+import { UserState } from "../../types/Login"
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: Infinity, refetchOnMount: true } },
@@ -29,7 +31,7 @@ export default {
   ],
 } as Meta<typeof ReviewCard>
 
-export const Primary = () => {
+export const PrimaryRejected = () => {
   const request: ModerationRequestResponse = {
     request_type: "appdata",
     request_data: {
@@ -56,9 +58,61 @@ export const Primary = () => {
     comment: null,
   }
 
+  const userInfo: UserState = {
+    loading: false,
+    info: {
+      permissions: [Permission.moderation],
+    },
+  }
+
   return (
-    <ReviewCard title={t("moderation-appstream")} request={request}>
-      <div>Show table here</div>
-    </ReviewCard>
+    <UserContext.Provider value={{ ...userInfo }}>
+      <ReviewCard title={t("moderation-appstream")} request={request}>
+        <div>Show table here</div>
+      </ReviewCard>
+    </UserContext.Provider>
+  )
+}
+
+export const Primary = () => {
+  const request: ModerationRequestResponse = {
+    request_type: "appdata",
+    request_data: {
+      keys: {
+        Name: "Test App",
+      },
+      current_values: {
+        Name: "Test App",
+      },
+    },
+    id: request_id,
+    app_id: "tv.abc.TestApp",
+    created_at: faker.date.past().toISOString(),
+
+    build_id: faker.number.int(),
+    job_id: faker.number.int(),
+    is_outdated: false,
+
+    is_new_submission: true,
+
+    handled_by: null,
+    handled_at: null,
+    is_approved: false,
+    comment: null,
+  }
+
+  const userInfo: UserState = {
+    loading: false,
+    info: {
+      permissions: [Permission.moderation],
+    },
+  }
+
+  return (
+    <UserContext.Provider value={{ ...userInfo }}>
+      <ReviewCard title={t("moderation-appstream")} request={request}>
+        <div>Show table here</div>
+      </ReviewCard>
+    </UserContext.Provider>
   )
 }
