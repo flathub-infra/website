3a5612aff09ec76461d8c5de7e3283b7ff9dbb5c
Razze <razzeee@gmail.com>
2024-10-13 17:22:50 +0200

    Use switch from shadcn
    Improve test behavior

---
+++ frontend/@/components/ui/Switch.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Switch } from "./switch"
+import { expect, within } from "@storybook/test"
+import { useArgs } from "@storybook/preview-api"
+import React from "react"
+
+const meta = {
+  title: "Components/Switch",
+  component: Switch,
+} satisfies Meta<typeof Switch>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Enabled: Story = {
+  args: {
+    checked: true,
+    onCheckedChange: () => {},
+  },
+  render: function Render(args) {
+    const [{ checked }, updateArgs] = useArgs()
+
+    function onChange() {
+      updateArgs({ checked: !checked })
+    }
+
+    return <Switch checked={checked} onCheckedChange={onChange} />
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const button = canvas.getByRole("switch")
+
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveAttribute("data-state", "checked")
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    checked: false,
+    onCheckedChange: () => {},
+  },
+  render: function Render(args) {
+    const [{ checked }, updateArgs] = useArgs()
+
+    function onChange() {
+      updateArgs({ checked: !checked })
+    }
+
+    return <Switch checked={checked} onCheckedChange={onChange} />
+  },
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const button = canvas.getByRole("switch")
+
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveAttribute("data-state", "unchecked")
+  },
+}
