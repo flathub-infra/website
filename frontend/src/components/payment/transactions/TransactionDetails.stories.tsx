ae975241d6cf335b17e54ddc16711e3bf29ed634
Razze <razzeee@gmail.com>
2024-10-28 00:57:03 +0100

    Make transaction details a bit nicer

---
+++ frontend/src/components/payment/transactions/TransactionDetails.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import TransactionDetails from "./TransactionDetails"
+
+const meta = {
+  component: TransactionDetails,
+  title: "Components/Payment/TransactionDetails",
+} satisfies Meta<typeof TransactionDetails>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    transaction: {
+      details: [
+        {
+          kind: "donation",
+          recipient: "https://www.flathub.org/",
+          amount: 100,
+          currency: "USD",
+        },
+        {
+          kind: "donation",
+          recipient: "https://www.flathub.org/",
+          amount: 100,
+          currency: "USD",
+        },
+      ],
+      summary: {
+        created: 1612119840,
+        currency: "USD",
+        id: "45",
+        kind: "donation",
+        reason: "payment",
+        status: "success",
+        updated: 1612119600,
+        value: 200,
+      },
+      receipt: "15",
+    },
+  },
+}
--- frontend/src/components/payment/transactions/TransactionDetails.tsx
+++ frontend/src/components/payment/transactions/TransactionDetails.tsx
@@ -1,34 +1,109 @@
 import { useTranslation } from "next-i18next"
 import { FunctionComponent } from "react"
 import { TransactionDetailed } from "../../../types/Payment"
-import TransactionPayout from "./TransactionPayout"
-import TransactionSummary from "./TransactionSummary"
+import { formatCurrency } from "../../../utils/localize"
+import { format } from "date-fns"
+import { getLocale } from "../../../localize"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { UTCDate } from "@date-fns/utc"
 
 interface Props {
   transaction: TransactionDetailed
 }
 
 const TransactionDetails: FunctionComponent<Props> = ({ transaction }) => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
 
-  const entries = transaction.details.map((entry) => {
-    return <TransactionPayout key={entry.recipient} payout={entry} />
-  })
+  const { created, updated, kind, value, status } = transaction.summary
 
   return (
     <div>
-      <div>
-        <h3 className="my-4 text-xl font-semibold">
-          {t("transaction-summary")}
-        </h3>
-        <TransactionSummary transaction={transaction} />
-      </div>
-      <div>
-        <h3 className="my-4 text-xl font-semibold">
-          {t("transaction-breakdown")}
-        </h3>
-        <div className="flex flex-col gap-3 rounded-xl bg-flathub-white p-3 shadow-md dark:bg-flathub-arsenic">
-          {entries}
+      <h3 className="my-4 text-xl font-semibold">{t("transaction-summary")}</h3>
+      <div className="flex flex-col gap-3 rounded-xl bg-flathub-white p-3 shadow-md dark:bg-flathub-arsenic">
+        <div className="grid grid-cols-3 md:grid-cols-6 gap-2 lg:max-w-6xl">
+          <div className="grid col-span-3 md:col-span-3 grid-cols-subgrid">
+            <div>{t("transaction-summary-type")}</div>
+            <div className="md:col-span-2">{t(`kind-${kind}`)}</div>
+          </div>
+          <div className="grid col-span-3 md:col-span-3 grid-cols-subgrid">
+            <div>{t("transaction-summary-status")}</div>
+            <div className="md:col-span-2">{t(`status-${status}`)}</div>
+          </div>
+          <div className="grid col-span-3 md:col-span-3 grid-cols-subgrid">
+            <div>{t("transaction-summary-created")}</div>
+            <div className="md:col-span-2">
+              {format(new UTCDate(created * 1000), "P p", {
+                locale: getLocale(i18n.language),
+              })}
+            </div>
+          </div>
+          <div className="grid col-span-3 md:col-span-3 grid-cols-subgrid">
+            <div>{t("transaction-summary-updated")}</div>
+            <div className="md:col-span-2">
+              {format(new UTCDate(updated * 1000), "P p", {
+                locale: getLocale(i18n.language),
+              })}
+            </div>
+          </div>
+          <div>
+            {transaction.receipt && (
+              <a
+                href={transaction.receipt}
+                className="no-underline hover:underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {t("stripe-receipt")}
+              </a>
+            )}
+          </div>
+        </div>
+        <div>
+          <h3 className="pt-6 my-4 text-xl font-semibold">{t("positions")}</h3>
+          <Table className="w-full lg:max-w-6xl">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">
+                  {t("position-short")}
+                </TableHead>
+                <TableHead>{t("type")}</TableHead>
+                <TableHead>{t("recipient")}</TableHead>
+                <TableHead className="text-right">{t("amount")}</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {transaction.details.map((entry, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell>{t(`kind-${entry.kind}`)}</TableCell>
+                  <TableCell>{entry.recipient}</TableCell>
+                  <TableCell className="text-right">
+                    {formatCurrency(
+                      entry.amount / 100,
+                      i18n.language,
+                      entry.currency,
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>{t("total")}</TableCell>
+                <TableCell className="text-right">
+                  {formatCurrency(value / 100, i18n.language)}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
         </div>
       </div>
     </div>
