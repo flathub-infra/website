d1f5ced96a5d833cdb577db0288a468aa388e874
Razze <razzeee@gmail.com>
2024-10-28 00:57:03 +0100

    Make transaction details a bit nicer

---
+++ frontend/src/components/payment/transactions/TransactionCancelButton.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import TransactionCancelButton from "./TransactionCancelButton"
+
+const meta = {
+  component: TransactionCancelButton,
+  title: "Components/Payment/TransactionCancelButton",
+} satisfies Meta<typeof TransactionCancelButton>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    id: "123",
+  },
+}
