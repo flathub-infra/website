9e2b04bfbde76f019c57a07e87f61745219e9c99
Kolja Lampe <razzeee@gmail.com>
2024-11-28 00:24:53 +0100

    Format files

--- frontend/@/components/ui/radio-group.tsx
+++ frontend/@/components/ui/radio-group.tsx
@@ -29,7 +29,7 @@ const RadioGroupItem = React.forwardRef<
       ref={ref}
       className={cn(
         "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        className,
       )}
       {...props}
     >
