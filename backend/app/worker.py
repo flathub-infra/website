87738e62ab838de59eb9a16e8f755a1bb73b73d5
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-01-23 21:07:44 +0100

    worker: Remove sqldb parameter from refresh_repo_list call

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -300,7 +300,7 @@ def refresh_github_repo_list(gh_access_token: str, accountId: int):
         if not gh_access_token:
             return
 
-        logins.refresh_repo_list(sqldb, gh_access_token, accountId)
+        logins.refresh_repo_list(gh_access_token, accountId)
         sqldb.session.commit()
 
 
