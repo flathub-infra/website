c02a6c0ff164da464f44de0eb7a2c1a607b9ff4b
Razze <razzeee@gmail.com>
2025-02-05 00:27:28 +0100

    Expand verification stories

--- frontend/src/components/application/AppVerificationControls/LoginVerification.stories.tsx
+++ frontend/src/components/application/AppVerificationControls/LoginVerification.stories.tsx
@@ -1,54 +1,67 @@
 import { faker } from "@faker-js/faker"
-import { Meta } from "@storybook/react"
-import React from "react"
+import { Meta, StoryObj } from "@storybook/react"
 import LoginVerification from "./LoginVerification"
-import { AvailableMethod } from "../../../codegen/model"
+import { expect, userEvent, waitFor, within } from "@storybook/test"
 
-export default {
-  title: "Components/Application/AppVerificationControls/LoginVerification",
+const meta = {
   component: LoginVerification,
-} as Meta<typeof LoginVerification>
+  title: "Components/Application/AppVerificationControls/LoginVerification",
+} satisfies Meta<typeof LoginVerification>
 
-export const Individual = () => {
-  const method: AvailableMethod = {
-    method: "login_provider",
-    login_provider: "github",
-    login_name: faker.internet.userName(),
-    login_is_organization: false,
-    login_status: "ready",
-  }
+export default meta
 
-  return (
-    <div className="space-y-3">
-      <LoginVerification
-        appId="io.github.appId"
-        isNewApp={false}
-        method={method}
-        onVerified={() => {}}
-        onReloadNeeded={() => {}}
-      ></LoginVerification>
-    </div>
-  )
+type Story = StoryObj<typeof meta>
+
+export const Individual: Story = {
+  args: {
+    appId: "io.github.appId",
+    isNewApp: false,
+    method: {
+      method: "login_provider",
+      login_provider: "github",
+      login_name: faker.internet.displayName(),
+      login_is_organization: false,
+      login_status: "ready",
+    },
+    onVerified: () => {},
+    onReloadNeeded: () => {},
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(
+      canvas.getByRole("button", { name: "Login provider verification" }),
+    )
+
+    await waitFor(() => {
+      expect(canvas.getByText("io.github.appId")).toBeInTheDocument()
+      expect(canvas.getByText("Verify app")).toBeInTheDocument()
+    })
+  },
 }
 
-export const Organization = () => {
-  const method: AvailableMethod = {
-    method: "login_provider",
-    login_provider: "github",
-    login_name: faker.internet.domainWord(),
-    login_is_organization: true,
-    login_status: "ready",
-  }
+export const Organization: Story = {
+  args: {
+    appId: "io.github.appId",
+    isNewApp: false,
+    method: {
+      method: "login_provider",
+      login_provider: "github",
+      login_name: faker.internet.domainWord(),
+      login_is_organization: true,
+      login_status: "ready",
+    },
+    onVerified: () => {},
+    onReloadNeeded: () => {},
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(
+      canvas.getByRole("button", { name: "Login provider verification" }),
+    )
 
-  return (
-    <div className="space-y-3">
-      <LoginVerification
-        appId="io.github.appId"
-        method={method}
-        isNewApp={false}
-        onVerified={() => {}}
-        onReloadNeeded={() => {}}
-      ></LoginVerification>
-    </div>
-  )
+    await waitFor(() => {
+      expect(canvas.getByText("Verify app")).toBeInTheDocument()
+      expect(canvas.getByText("io.github.appId")).toBeInTheDocument()
+    })
+  },
 }
