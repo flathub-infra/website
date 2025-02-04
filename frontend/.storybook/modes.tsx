434dec5fde2c9d07f5b107308afb2556601f733d
Razze <razzeee@gmail.com>
2025-02-04 23:25:14 +0100

    Add chromatic modes

---
+++ frontend/.storybook/modes.tsx
@@ -0,0 +1,16 @@
+export const allModes = {
+  mobile: {
+    viewport: "small",
+  },
+  desktop: {
+    viewport: "large",
+  },
+  dark: {
+    backgrounds: { value: "#251f32" },
+    theme: "dark",
+  },
+  light: {
+    backgrounds: { value: "#fafafa" },
+    theme: "light",
+  },
+}
--- frontend/src/components/Disclosure.stories.tsx
+++ frontend/src/components/Disclosure.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react"
 
 import { FlathubDisclosure } from "./Disclosure"
 import React from "react"
+import { expect, userEvent, waitFor, within } from "@storybook/test"
 
 const meta: Meta<typeof FlathubDisclosure> = {
   title: "Components/Disclosure",
@@ -11,9 +12,24 @@ const meta: Meta<typeof FlathubDisclosure> = {
 export default meta
 type Story = StoryObj<typeof FlathubDisclosure>
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     buttonItems: [<div key="button-1">Button 1</div>],
     children: [<div key="content-1">Content</div>],
+  },
+}
+
+export const Opened: Story = {
+  args: {
+    buttonItems: [<div key="button-1">Button 1</div>],
+    children: [<div key="content-1">Content</div>],
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(canvas.getByRole("button"))
+
+    await waitFor(() => {
+      expect(canvas.getByText("Content")).toBeInTheDocument()
+    })
   },
 }
