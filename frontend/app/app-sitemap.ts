1c9ce71c71548b065e8797a7fc8588799d5b16ed
Kolja Lampe <razzeee@gmail.com>
2025-05-24 22:43:27 +0200

    Make sure to use UTCDate for dates from the backend

--- frontend/app/app-sitemap.ts
+++ frontend/app/app-sitemap.ts
@@ -1,3 +1,4 @@
+import { UTCDate } from "@date-fns/utc"
 import { getServerSideSitemap } from "next-sitemap"
 import { notFound } from "next/navigation"
 import { languages } from "src/localize"
@@ -25,7 +26,7 @@ export async function createAppSitemap(chunk: number) {
       .map((appId, i) => ({
         loc: `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/apps/${appId}`,
         // todo - add more fields here, maybe get lastmod from summary
-        lastmod: new Date().toISOString(),
+        lastmod: new UTCDate().toISOString(),
         alternateRefs: languages?.map((lang) => ({
           href: `${process.env.NEXT_PUBLIC_SITE_BASE_URI}/${lang}/apps/${appId}`,
           hreflang: lang,
