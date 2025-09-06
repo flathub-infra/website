60902bf1275798f5e04ed5177f0afd9f9577475f
Kolja Lampe <razzeee@gmail.com>
2025-09-06 23:35:06 +0200

    Animate distro logos and names

--- frontend/app/[locale]/setup/[distro]/distro-setup-client.tsx
+++ frontend/app/[locale]/setup/[distro]/distro-setup-client.tsx
@@ -5,6 +5,7 @@ import Breadcrumbs from "../../../../src/components/Breadcrumbs"
 import { DistroSetup } from "../../../../src/distro-setup"
 import { distroMap } from "../../../../src/components/setup/Distros"
 import React from "react"
+import { LayoutGroup } from "framer-motion"
 
 interface Props {
   distroData: DistroSetup
@@ -30,16 +31,18 @@ export default function DistroSetupClient({ distroData, locale }: Props) {
   ]
 
   return (
-    <div className="max-w-11/12 mx-auto w-11/12 space-y-10 pt-4 2xl:w-[1400px] 2xl:max-w-[1400px]">
-      <div className="mx-auto max-w-2xl">
-        <Breadcrumbs pages={pages} />
-      </div>
+    <LayoutGroup>
+      <div className="max-w-11/12 mx-auto w-11/12 space-y-10 pt-4 2xl:w-[1400px] 2xl:max-w-[1400px]">
+        <div className="mx-auto max-w-2xl">
+          <Breadcrumbs pages={pages} />
+        </div>
 
-      <div className="prose mx-auto dark:prose-invert prose-pre:rounded-xl">
-        <div key={distroData.name} className="space-y-4">
-          {distroMap(locale).get(distroData.name.replaceAll("/", ""))}
+        <div className="prose mx-auto dark:prose-invert prose-pre:rounded-xl">
+          <div key={distroData.name} className="space-y-4">
+            {distroMap(locale).get(distroData.name.replaceAll("/", ""))}
+          </div>
         </div>
       </div>
-    </div>
+    </LayoutGroup>
   )
 }
