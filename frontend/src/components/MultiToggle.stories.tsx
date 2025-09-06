1e80f53074eb9866e5bf4968bc50ba127cedf7f3
Kolja Lampe <razzeee@gmail.com>
2025-09-06 23:40:13 +0200

    Fix lint and streamline layout files

--- frontend/src/components/MultiToggle.stories.tsx
+++ frontend/src/components/MultiToggle.stories.tsx
@@ -16,7 +16,7 @@ export const SmallWithIcons: Story = {
     size: "sm",
     variant: "primary",
   },
-  render: (args) => {
+  render: function SmallWithIconsRender(args) {
     const [selectedToggle, setSelectedToggle] = useState<boolean | undefined>(
       false,
     )
@@ -59,7 +59,7 @@ export const LargePrimary: Story = {
     size: "lg",
     variant: "primary",
   },
-  render: (args) => {
+  render: function LargePrimaryRender(args) {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const options = [{ name: "One" }, { name: "Two" }, { name: "Three" }]
 
@@ -82,7 +82,7 @@ export const LargeSecondary: Story = {
     size: "lg",
     variant: "secondary",
   },
-  render: (args) => {
+  render: function LargeSecondaryRender(args) {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const options = [{ name: "One" }, { name: "Two" }, { name: "Three" }]
 
@@ -105,7 +105,7 @@ export const LargeFlat: Story = {
     size: "lg",
     variant: "flat",
   },
-  render: (args) => {
+  render: function LargeFlatRender(args) {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const options = [
       { name: "Newest" },
