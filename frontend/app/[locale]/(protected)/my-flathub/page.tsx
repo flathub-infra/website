6f08437bf35f5c7085c861c3a798cb261f35ee93
Bart Piotrowski <b@bpiotrowski.pl>
2025-09-08 15:27:44 +0200

    Revert "Cleanup some params"
    This reverts commit cd99e3db39a945d75e47ba9c32ed84e5065aacd6.

--- frontend/app/[locale]/(protected)/my-flathub/page.tsx
+++ frontend/app/[locale]/(protected)/my-flathub/page.tsx
@@ -2,7 +2,11 @@ import { Metadata } from "next"
 import { getTranslations } from "next-intl/server"
 import MyFlathubClient from "./my-flathub-client"
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>
+}): Promise<Metadata> {
   const t = await getTranslations()
 
   return {
