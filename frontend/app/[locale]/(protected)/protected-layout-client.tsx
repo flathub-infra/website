64ff65088c3bbe403975585efa49a110e97ad528
Bart Piotrowski <b@bpiotrowski.pl>
2025-09-08 20:52:50 +0200

    Revert "Revert "Revert "Fix lint and streamline layout files"""
    This reverts commit fa1be069a1f4f7dfc8459ffb888bcc2ef25263ff.

--- frontend/app/[locale]/(protected)/protected-layout-client.tsx
+++ frontend/app/[locale]/(protected)/protected-layout-client.tsx
@@ -1,19 +1,45 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ReactNode, useEffect } from "react"
+import { useRouter, usePathname } from "src/i18n/navigation"
 import { useUserContext } from "../../../src/context/user-info"
+import { UserInfo } from "../../../src/codegen"
 import Spinner from "../../../src/components/Spinner"
 
 interface ProtectedLayoutClientProps {
   children: ReactNode
+  locale: string
+  serverUser: UserInfo | null
 }
 
 export default function ProtectedLayoutClient({
   children,
+  locale,
+  serverUser,
 }: ProtectedLayoutClientProps) {
+  const router = useRouter()
+  const pathname = usePathname()
   const user = useUserContext()
 
-  if (user.loading || user.info === undefined) {
+  useEffect(() => {
+    // If server-side couldn't get user info, fall back to client-side check
+    if (!serverUser && !user.info && !user.loading) {
+      // Redirect to login with return URL
+      router.replace(`/login?returnTo=${encodeURIComponent(pathname)}`)
+    }
+  }, [serverUser, user, router, pathname])
+
+  // Show loading while checking authentication
+  if (!serverUser && user.loading) {
+    return (
+      <div className="flex justify-center items-center min-h-[50vh]">
+        <Spinner size="m" />
+      </div>
+    )
+  }
+
+  // If neither server nor client has user info, don't render (redirect is happening)
+  if (!serverUser && !user.info) {
     return (
       <div className="flex justify-center items-center min-h-[50vh]">
         <Spinner size="m" />
