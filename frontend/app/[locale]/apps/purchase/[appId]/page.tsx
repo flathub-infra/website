9d9a04d078a9012b6fd052d2ea35b264ac1213c7
Bart Piotrowski <b@bpiotrowski.pl>
2025-09-08 20:52:50 +0200

    Revert "Revert "Revert "Fix lint for title"""
    This reverts commit a4b40fec7f54100479baf469fd708cdaf286b16d.

--- frontend/app/[locale]/apps/purchase/[appId]/page.tsx
+++ frontend/app/[locale]/apps/purchase/[appId]/page.tsx
@@ -3,7 +3,7 @@ import { fetchAppstream, fetchVendingConfig } from "../../../../../src/fetchers"
 import { Metadata } from "next"
 import AppPurchaseClient from "./app-purchase-client"
 import { getTranslations } from "next-intl/server"
-import { getAppVendingSetupVendingappAppIdSetupGet } from "src/codegen/vending/vending"
+import { useGetAppVendingSetupVendingappAppIdSetupGet } from "src/codegen/vending/vending"
 
 interface Props {
   params: Promise<{
@@ -23,12 +23,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     }
   }
 
-  const vendingSetup = await getAppVendingSetupVendingappAppIdSetupGet(app.id, {
-    withCredentials: true,
+  const vendingSetup = useGetAppVendingSetupVendingappAppIdSetupGet(app.id, {
+    axios: { withCredentials: true },
+    query: {
+      enabled: !!app.id,
+    },
   })
 
   // When the minimum payment is 0, the application does not require payment
-  const isDonationOnly = vendingSetup.data.minimum_payment === 0
+  const isDonationOnly = vendingSetup.data.data.minimum_payment === 0
 
   const title = t(isDonationOnly ? "kind-donate-app" : "kind-purchase-app", {
     appName: app?.name,
