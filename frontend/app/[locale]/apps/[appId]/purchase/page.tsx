44748b0ae6b09d3a9f273e642e2abb1f26da8768
Bart Piotrowski <b@bpiotrowski.pl>
2025-09-08 15:27:44 +0200

    Revert "Correctly determine metadata based on donation/purchase kind"
    This reverts commit 0f998d7e51a3254488b251c13451a2afcd46db9d.

--- frontend/app/[locale]/apps/[appId]/purchase/page.tsx
+++ frontend/app/[locale]/apps/[appId]/purchase/page.tsx
@@ -3,7 +3,6 @@ import { fetchAppstream, fetchVendingConfig } from "../../../../../src/fetchers"
 import { Metadata } from "next"
 import PurchaseClient from "./purchase-client"
 import { getTranslations } from "next-intl/server"
-import { getAppVendingSetupVendingappAppIdSetupGet } from "src/codegen"
 
 interface Props {
   params: Promise<{
@@ -23,17 +22,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     }
   }
 
-  const vendingSetup = await getAppVendingSetupVendingappAppIdSetupGet(app.id, {
-    withCredentials: true,
-  })
-
-  // When the minimum payment is 0, the application does not require payment
-  const isDonationOnly = vendingSetup.data.minimum_payment === 0
+  // We'll need to check if it's donation only, but for now use a generic title
+  // This will be refined in the client component
+  const title = t("kind-purchase-app", { appName: app.name })
 
   return {
-    title: t(isDonationOnly ? "kind-donate-app" : "kind-purchase-app", {
-      appName: app?.name,
-    }),
+    title,
     robots: {
       index: false,
       follow: false,
