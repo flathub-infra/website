f98c5478ee3262ae291623d0e07e2869ef6d29e1
Kolja <razzeee@gmail.com>
2025-09-10 09:32:07 +0200

    Revert "Refactor cache handler to use @fortedigital/nextjs-cache-handler and …"
    This reverts commit 34abd45912a77034dd0b9a0b27fa1f97bfb9f37b.

--- frontend/cache-handler.mjs
+++ frontend/cache-handler.mjs
@@ -1,98 +1,57 @@
 //@ts-check
-import { createClient } from "redis"
-import { PHASE_PRODUCTION_BUILD } from "next/constants.js"
-import { CacheHandler } from "@fortedigital/nextjs-cache-handler"
-import createLruHandler from "@fortedigital/nextjs-cache-handler/local-lru"
-import createRedisHandler from "@fortedigital/nextjs-cache-handler/redis-strings"
 
-CacheHandler.onCreation(({ buildId }) => {
+import { CacheHandler } from "@neshca/cache-handler"
+import createRedisHandler from "@neshca/cache-handler/redis-strings"
+import createFallbackHandler from "@neshca/cache-handler/local-lru"
+import { promiseWithTimeout } from "@neshca/cache-handler/helpers"
+import { createClient } from "redis"
+
+CacheHandler.onCreation(async ({ buildId }) => {
   if (!buildId) {
     return {
       handlers: [],
     }
   }
 
-  // Important - It's recommended to use global scope to ensure only one Redis connection is made
-  // This ensures only one instance get created
-  if (global.cacheHandlerConfig) {
-    return global.cacheHandlerConfig
-  }
+  const client = createClient({
+    url: process.env.REDIS_URL ?? "redis://localhost:6379",
+  })
 
-  // Important - It's recommended to use global scope to ensure only one Redis connection is made
-  // This ensures new instances are not created in a race condition
-  if (global.cacheHandlerConfigPromise) {
-    return global.cacheHandlerConfigPromise
-  }
+  // Ignore Redis errors: https://github.com/redis/node-redis?tab=readme-ov-file#events
+  client.on("error", () => {})
 
-  // Main promise initializing the handler
-  global.cacheHandlerConfigPromise = (async () => {
-    let redisClient = null
+  let handler
 
-    if (PHASE_PRODUCTION_BUILD !== process.env.NEXT_PHASE) {
-      const settings = {
-        url: process.env.REDIS_URL ?? "redis://localhost:6379",
-        pingInterval: 10000,
-      }
+  try {
+    console.info("Connecting to Redis...")
 
-      try {
-        redisClient = createClient(settings)
-        redisClient.on("error", (e) => {
-          if (typeof process.env.NEXT_PRIVATE_DEBUG_CACHE !== "undefined") {
-            console.warn("Redis error", e)
-          }
-          global.cacheHandlerConfig = null
-          global.cacheHandlerConfigPromise = null
-        })
-      } catch (error) {
-        console.warn("Failed to create Redis client:", error)
-      }
-    }
+    await promiseWithTimeout(client.connect(), 1000)
 
-    if (redisClient) {
-      try {
-        console.info("Connecting Redis client...")
-        await redisClient.connect()
-        console.info("Redis client connected.")
-      } catch (error) {
-        console.warn("Failed to connect Redis client:", error)
-        await redisClient
-          .disconnect()
-          .catch(() =>
-            console.warn(
-              "Failed to quit the Redis client after failing to connect.",
-            ),
-          )
-      }
-    }
+    console.info("Connected to Redis.")
 
-    const lruCache = createLruHandler()
-
-    if (!redisClient?.isReady) {
-      console.error("Failed to initialize caching layer.")
-      global.cacheHandlerConfigPromise = null
-      global.cacheHandlerConfig = { handlers: [lruCache] }
-      return global.cacheHandlerConfig
-    }
-
-    const redisCacheHandler = createRedisHandler({
-      client: redisClient,
+    handler = createRedisHandler({
+      client,
       keyPrefix: buildId,
       timeoutMs: 500,
     })
-
-    global.cacheHandlerConfigPromise = null
-
-    // This example uses composite handler to switch from Redis to LRU cache if tags contains `memory-cache` tag.
-    // You can skip composite and use Redis or LRU only.
-    global.cacheHandlerConfig = {
-      handlers: [redisCacheHandler],
-      ttl: { estimateExpireAge: (staleAge) => staleAge + 60 },
+  } catch (error) {
+    if (client.isOpen) {
+      await client.disconnect()
     }
 
-    return global.cacheHandlerConfig
-  })()
+    console.warn("Failed to connect to Redis:", error)
 
-  return global.cacheHandlerConfigPromise
+    console.warn("Falling back to local LRU cache.")
+
+    handler = createFallbackHandler()
+  }
+
+  return {
+    handlers: [handler],
+    ttl: {
+      estimateExpireAge: (staleAge) => staleAge + 60,
+    },
+  }
 })
 
 export default CacheHandler
