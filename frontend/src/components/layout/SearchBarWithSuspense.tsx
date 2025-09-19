116229b3058b17275e4999604324136d0bfe5ea3
Razze <razzeee@gmail.com>
2025-09-19 20:24:21 +0200

    Align search bar skeleton a bit better

--- frontend/src/components/layout/SearchBarWithSuspense.tsx
+++ frontend/src/components/layout/SearchBarWithSuspense.tsx
@@ -2,15 +2,24 @@
 
 import { Suspense } from "react"
 import SearchBar from "./SearchBar"
+import { HiMagnifyingGlass } from "react-icons/hi2"
 
 const SearchBarSkeleton = () => (
   <div className="relative">
     <div className="absolute inset-y-0 start-0 flex items-center ps-2">
       <div className="rounded-full p-1 opacity-50">
-        <div className="size-5 bg-gray-300 rounded animate-pulse" />
+        <HiMagnifyingGlass className="size-5 text-flathub-spanish-gray" />
       </div>
     </div>
-    <div className="block w-full rounded-full bg-flathub-gainsborow/50 py-2 ps-10 pe-2 h-10 animate-pulse" />
+    <div className="block w-full rounded-full bg-flathub-gainsborow/50 py-2 ps-10 pe-2 h-9 animate-pulse" />
+    <div className="pointer-events-none absolute inset-y-0 end-0 hidden items-center pe-5 peer-focus:hidden md:flex">
+      <kbd
+        className="flex size-5 items-center justify-center rounded border-2 border-flathub-gray-x11/60 font-sans text-xs text-flathub-arsenic dark:border-flathub-sonic-silver dark:text-flathub-gainsborow"
+        aria-hidden="true"
+      >
+        /
+      </kbd>
+    </div>
   </div>
 )
 
