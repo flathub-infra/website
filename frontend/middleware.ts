1185093fa8ea7852d8a493d0543bb5ba0ab3fba6
Kolja Lampe <razzeee@gmail.com>
2025-09-19 11:03:50 +0200

    Revert "Cleanup middleware"
    This reverts commit de16ce1649c453a44bcfef4916baa8d40649af27.

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,11 +1,14 @@
 import createMiddleware from "next-intl/middleware"
+import { NextRequest, NextResponse } from "next/server"
 import { routing } from "src/i18n/routing"
 
-export default createMiddleware(routing)
+export default async function middleware(request: NextRequest) {
+  return createMiddleware(routing)(request) || NextResponse.next()
+}
 
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher: "/((?!api|_next|_vercel|.*\\..*).*)",
 }
