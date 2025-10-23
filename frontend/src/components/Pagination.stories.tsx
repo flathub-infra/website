14ed25cf92b709d5f760b68b7e2f4c58d6d56319
Kolja Lampe <razzeee@gmail.com>
2025-10-23 10:03:16 +0200

    fix(Pagination): Fix broken pagination stories

--- frontend/src/components/Pagination.stories.tsx
+++ frontend/src/components/Pagination.stories.tsx
@@ -1,32 +1,66 @@
-import React from "react"
-import { Meta } from "@storybook/nextjs-vite"
+import type { Meta, StoryObj } from "@storybook/nextjs-vite"
 import Pagination from "./Pagination"
 
-export default {
+const meta = {
   title: "Components/Pagination",
   component: Pagination,
-} as Meta<typeof Pagination>
+  tags: ["autodocs"],
+} satisfies Meta<typeof Pagination>
 
-export const all = () => {
-  return (
-    <>
-      {/* this one should be hidden and not show up */}
-      <Pagination currentPage={1} pages={[1]} />
-      <Pagination currentPage={3} pages={[1, 2, 3, 4, 5]} />
-      <Pagination
-        currentPage={12}
-        pages={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-      />
-      <Pagination
-        currentPage={3}
-        pages={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-      />
-      {/* With query params (admin pages) */}
-      <Pagination
-        currentPage={3}
-        pages={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-        useQueryParams
-      />
-    </>
-  )
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const mockPathname = "/apps/page/3"
+const mockSearchParams = new URLSearchParams("foo=bar")
+
+// This story demonstrates that pagination with only 1 page should be hidden
+export const SinglePage: Story = {
+  args: {
+    currentPage: 1,
+    pages: [1],
+    pathname: mockPathname,
+    searchParams: mockSearchParams,
+  },
+}
+
+// Simple pagination with 5 pages, current page is 3
+export const FewPages: Story = {
+  args: {
+    currentPage: 3,
+    pages: [1, 2, 3, 4, 5],
+    pathname: mockPathname,
+    searchParams: mockSearchParams,
+  },
+}
+
+// Pagination with many pages, current page is at the end
+export const ManyPagesAtEnd: Story = {
+  args: {
+    currentPage: 12,
+    pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    pathname: mockPathname,
+    searchParams: mockSearchParams,
+  },
+}
+
+// Pagination with many pages, current page is near the beginning
+export const ManyPagesNearStart: Story = {
+  args: {
+    currentPage: 3,
+    pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    pathname: mockPathname,
+    searchParams: mockSearchParams,
+  },
+}
+
+// Pagination using query params (for admin pages)
+export const WithQueryParams: Story = {
+  args: {
+    currentPage: 3,
+    pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    useQueryParams: true,
+    pathname: mockPathname,
+    searchParams: mockSearchParams,
+  },
 }
