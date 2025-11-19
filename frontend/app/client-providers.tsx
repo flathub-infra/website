6a06a937535b5624d14d9c29975319f3b6d1fe71
Kolja Lampe <razzeee@gmail.com>
2025-11-19 17:10:59 +0100

    Import axios configuration in ClientProviders and simplify request interceptor logic

--- frontend/app/client-providers.tsx
+++ frontend/app/client-providers.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import "src/utils/axios-config"
 import { ReactNode } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider } from "next-themes"
