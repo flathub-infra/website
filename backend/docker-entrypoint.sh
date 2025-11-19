377e74a63ba145fa41d7ecca1dfb6fb206b55940
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-11-19 13:21:31 +0100

    Replace uvicorn with granian

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 alembic upgrade heads
-uvicorn app.main:router --host 0.0.0.0 --port 8000 --forwarded-allow-ips='*' $@
+granian --interface asgi app.main:router --host 0.0.0.0 --port 8000 --workers ${WEB_CONCURRENCY:-1} --forwarded-allow-ips '*' --access-log $@
