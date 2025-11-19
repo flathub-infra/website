52423b8ee56e5fe4f375a27de14ae471ec788e3e
Kolja Lampe <razzeee@gmail.com>
2025-11-19 17:10:59 +0100

    Import axios configuration in ClientProviders and simplify request interceptor logic

--- frontend/src/utils/axios-config.ts
+++ frontend/src/utils/axios-config.ts
@@ -11,16 +11,7 @@ axios.defaults.paramsSerializer = {
 
 axios.interceptors.request.use(
   (config) => {
-    if (!config.baseURL) {
-      const isAuthEndpoint = config.url?.startsWith("/auth")
-
-      if (isAuthEndpoint) {
-        config.baseURL =
-          process.env.NEXT_PUBLIC_API_BASE_URI || "https://flathub.org/api/v2"
-      } else {
-        config.baseURL = getApiBaseUrl()
-      }
-    }
+    config.baseURL = getApiBaseUrl()
     return config
   },
   (error) => {
