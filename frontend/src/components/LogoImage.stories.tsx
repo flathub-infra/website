ab388b6ed6436989de3af8a514f4229fb779a9cc
Kolja Lampe <razzeee@gmail.com>
2025-11-24 16:10:05 +0100

    fix(logo): update style properties for responsive image sizing

--- frontend/src/components/LogoImage.stories.tsx
+++ frontend/src/components/LogoImage.stories.tsx
@@ -19,3 +19,46 @@ export const Default: Story = {
     size: "128",
   },
 }
+
+export const Small: Story = {
+  args: {
+    appName: "Kodi",
+    iconUrl:
+      "https://dl.flathub.org/media/tv/kodi/Kodi/4f8cbfae09dc6c8c55501a5d3f604fbb/icons/128x128/tv.kodi.Kodi.png",
+    size: "24",
+  },
+}
+
+export const Medium: Story = {
+  args: {
+    appName: "Kodi",
+    iconUrl:
+      "https://dl.flathub.org/media/tv/kodi/Kodi/4f8cbfae09dc6c8c55501a5d3f604fbb/icons/128x128/tv.kodi.Kodi.png",
+    size: "64",
+  },
+}
+
+export const Large: Story = {
+  args: {
+    appName: "Kodi",
+    iconUrl:
+      "https://dl.flathub.org/media/tv/kodi/Kodi/4f8cbfae09dc6c8c55501a5d3f604fbb/icons/128x128/tv.kodi.Kodi.png",
+    size: "256",
+  },
+}
+
+export const Fallback: Story = {
+  args: {
+    appName: "Kodi",
+    iconUrl: "",
+    size: "128",
+  },
+}
+
+export const ExternalUrl: Story = {
+  args: {
+    appName: "Kodi",
+    iconUrl: "https://example.com/icon.png",
+    size: "128",
+  },
+}
