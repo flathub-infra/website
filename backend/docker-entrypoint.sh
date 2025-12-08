881e946261fc8c2d67fab18e6304eba5f48b5037
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2025-12-08 10:30:41 +0100

    Add more flags to stabilize granian

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 alembic upgrade heads
-granian --interface asgi app.main:app --host 0.0.0.0 --port 8000 --workers ${WEB_CONCURRENCY:-1} --access-log $@
+granian --interface asgi app.main:app --host 0.0.0.0 --port 8000 \
+  --no-http1-keep-alive \
+  --workers-lifetime 3600 \
+  --respawn-failed-workers \
+  --workers-kill-timeout 30 \
+  --access-log $@
