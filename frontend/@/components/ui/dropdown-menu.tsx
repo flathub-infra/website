b6da55e198738a1303dcf02b8634fe4126a550d3
Razze <razzeee@gmail.com>
2026-02-03 03:36:31 +0100

    Use unified radix package

--- frontend/@/components/ui/dropdown-menu.tsx
+++ frontend/@/components/ui/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
+import { DropdownMenu as DropdownMenuPrimitive } from "radix-ui"
 import { cn } from "@/lib/utils"
 import {
   CheckIcon,
