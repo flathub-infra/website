6fa730c001edd8a7922c23a5ce2037595840eac2
Cameron Radmore <radmorecameron@gmail.com>
2026-04-06 20:56:21 -0400

    feat(frontend): Add HealthFitness category

--- frontend/app/[locale]/(home)/page.tsx
+++ frontend/app/[locale]/(home)/page.tsx
@@ -36,6 +36,7 @@ const categoryOrder: MainCategory[] = [
   MainCategory.network,
   MainCategory.development,
   MainCategory.science,
+  MainCategory.healthfitness,
   MainCategory.system,
   MainCategory.utility,
 ]
