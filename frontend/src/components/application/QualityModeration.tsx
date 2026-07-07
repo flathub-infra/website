5a27ab0b7042fb4f1b0bfe517822d58a929ae84f
Kolja Lampe <razzeee@gmail.com>
2026-07-07 23:41:49 +0200

    Fix linting

--- frontend/src/components/application/QualityModeration.tsx
+++ frontend/src/components/application/QualityModeration.tsx
@@ -293,7 +293,8 @@ export const QualityModeration = ({
           {canManageApp && (
             <Button size="lg" variant="secondary" asChild className="me-2">
               <Link href={`/apps/manage/${app.id}`}>
-                <Cog6ToothIcon className="size-5" /> <div className="hidden sm:block">{t("manage")}</div>
+                <Cog6ToothIcon className="size-5" />
+                <div className="hidden sm:block">{t("manage")}</div>
               </Link>
             </Button>
           )}
