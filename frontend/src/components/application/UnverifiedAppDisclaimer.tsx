419099fdcf40af12be105332bfce085c2ec0a17c
Bart Piotrowski <b@bpiotrowski.pl>
2026-07-17 13:12:32 +0200

    Add unverified app disclaimer component

---
+++ frontend/src/components/application/UnverifiedAppDisclaimer.tsx
@@ -0,0 +1,40 @@
+import { Alert, AlertDescription } from "@/components/ui/alert"
+import { InfoIcon } from "lucide-react"
+import { useTranslations } from "next-intl"
+
+interface Props {
+  developerName?: string | null
+  isExtraData: boolean
+  isVerified: boolean
+}
+
+export function UnverifiedAppDisclaimer({
+  developerName,
+  isExtraData,
+  isVerified,
+}: Props) {
+  const t = useTranslations()
+
+  if (isVerified) {
+    return null
+  }
+
+  const developer =
+    developerName?.trim() || t("unverified-app-disclaimer-unknown-developer")
+
+  return (
+    <Alert className="xl:max-w-[75%]">
+      <InfoIcon />
+      <AlertDescription>
+        <p>
+          {t(
+            isExtraData
+              ? "unverified-app-disclaimer-wrapper"
+              : "unverified-app-disclaimer-package",
+            { developer },
+          )}
+        </p>
+      </AlertDescription>
+    </Alert>
+  )
+}
