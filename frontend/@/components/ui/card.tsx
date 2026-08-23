69b5961768fd73fb4528348463c6f4196024371a
Razze <razzeee@gmail.com>
2026-08-23 18:02:02 +0200

    Revert "Use text balance in some places"
    This reverts commit 28ace5b85ad78eaee5ac84d2bc472ee08e247e93.

--- frontend/@/components/ui/card.tsx
+++ frontend/@/components/ui/card.tsx
@@ -32,7 +32,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("text-balance font-semibold leading-none", className)}
+      className={cn("leading-none font-semibold", className)}
       {...props}
     />
   )
