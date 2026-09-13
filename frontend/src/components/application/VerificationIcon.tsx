2385013e06bf1cc8065e92ece53f1b07d62641f5
Razze <razzeee@gmail.com>
2026-09-13 18:13:41 +0200

    Keep verification tooltip clear of developer links

--- frontend/src/components/application/VerificationIcon.tsx
+++ frontend/src/components/application/VerificationIcon.tsx
@@ -39,7 +39,7 @@ const VerificationIcon: FunctionComponent<Props> = ({
             </button>
           </TooltipTrigger>
 
-          <TooltipContent side="right" className="max-w-xs">
+          <TooltipContent side="top" className="max-w-xs">
             {verificationStatus.method === "manual"
               ? t.rich("verified-manually-tooltip", {
                   appid: (chunks) => <b>{appId}</b>,
